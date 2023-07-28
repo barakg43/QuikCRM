482a8043725b0fa6f93d84280af77e4dfae1e797
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-07-28 14:43:52 +0300

    add first 2 http endpoint

--- Backend/server/src/main/java/com/quik/server/http/CustomerHttpService.java
+++ Backend/server/src/main/java/com/quik/server/http/CustomerHttpService.java
@@ -2,7 +2,9 @@ package com.quik.server.http;
 
 import com.quik.server.sql.SqlClient;
 import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.quik.server.ServerConstants.SQL_YAML_CONFIG_LOCATION;
@@ -19,12 +21,12 @@ public class CustomerHttpService {
         sqlClient.createSqlConnection();
     }
 
-
-    public String getCostumerNameByID(int id){
+    @GetMapping("/customer/name")
+    public String getCostumerNameByID(@RequestParam int id){
         return sqlClient.getCostumerNameByID(id);
     }
-
-    public String getSupplierNameByID(int id){
+    @GetMapping("/supplier/name")
+    public String getSupplierNameByID(@RequestParam int id){
         return sqlClient.getSupplierNameByID(id);
     }
 }
