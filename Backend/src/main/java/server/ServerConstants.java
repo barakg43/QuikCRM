85033744a27812206e3cfc7ca0ee327009e60867
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-11-29 17:12:45 +0200

    move more files

---
+++ Backend/src/main/java/server/ServerConstants.java
@@ -0,0 +1,9 @@
+package server;
+
+public class ServerConstants {
+
+    public static final String SQL_CLIENT_LOGGER_NAME = "sql-client-logger";
+    public static final String REQUEST_LOGGER_NAME = "request-logger";
+    public static final String SQL_YAML_CONFIG_LOCATION = "C:\\ComputerScience\\litos-crm-config\\config\\sql-config.yaml";
+
+}
---
+++ Backend/src/main/java/server/sql/function/SqlFunction.java
@@ -0,0 +1,39 @@
+package server.sql.function;
+
+public class SqlFunction<T> {
+    private final String sqlFunctionQuery;
+    private final String argumentString;
+    private final Class<T> returnType;
+
+    //    public SqlFunction(String sqlFunctionQuery, String argumentString) {
+//
+//        this.sqlFunctionQuery = sqlFunctionQuery;
+//        this.argumentString = argumentString;
+//        this.returnType = (Class<T>) new TypeToken<T>() {}.get();
+//        System.out.println(returnType);
+//    }
+    public SqlFunction(String sqlFunctionQuery, String argumentString, Class<T> returnTye) {
+
+        this.sqlFunctionQuery = sqlFunctionQuery;
+        this.argumentString = argumentString;
+        this.returnType = returnTye;
+    }
+
+    public String getSqlFunctionQuery() {
+        return sqlFunctionQuery;
+    }
+
+    public String getArgumentString() {
+        return argumentString;
+    }
+
+
+    public Class<T> getReturnType() {
+        return returnType;
+    }
+
+    @Override
+    public String toString() {
+        return sqlFunctionQuery;
+    }
+}
