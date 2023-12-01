0d67b69f23b322df42817fd5ac7ba652a191424d
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-12-01 15:20:33 +0200

    few changes in pom ,add context path to vite

---
+++ Backend/src/main/server/http/WebAppConfig.java
@@ -0,0 +1,19 @@
+package main.server.http;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
+//@Configuration
+//@EnableWebMvc
+//
+//
+//    public class WebAppConfig extends WebMvcConfigurerAdapter {
+//
+//        @Override
+//        public void addViewControllers(ViewControllerRegistry registry) {
+//            registry.addRedirectViewController("/", "index.html");
+//        }
+//
+//    }
+
