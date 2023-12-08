9e4ad4cc5bd3e1b5e7d2d88ef1cd342838ec335d
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-12-08 15:50:43 +0200

    create application routes in AppRoutes

--- Frontend/src/app/AppRoutes.tsx
+++ Frontend/src/app/AppRoutes.tsx
@@ -1,10 +1,26 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-
+import Reminders from "../pages/Reminders";
+import Activities from "../pages/Activities";
+import Clients from "../pages/Clients";
+import Reports from "../pages/Reports";
+import Settings from "../pages/Settings";
+import SignUp from "../pages/Signup";
+import Login from "./../pages/Login.tsx";
+import PageNotFound from "../pages/PageNotFound";
+import Account from "../pages/Account.tsx";
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='remainders'></Route>
+        <Route path='reminders' element={<Reminders />} />
+        <Route path='activities' element={<Activities />} />
+        <Route path='clients' element={<Clients />} />
+        <Route path='reports' element={<Reports />} />
+        <Route path='settings' element={<Settings />} />
+        <Route path='login' element={<Login />} />
+        <Route path='signup' element={<SignUp />} />
+        <Route path='account' element={<Account />} />
+        <Route path='*' element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
