53451b38b643b9a412df87397e80bd0818be986a
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-12-11 22:35:43 +0200

    create components for app layout

---
+++ Frontend/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  background-color: var(--color-amber-700);
+`;
+
+function Header() {
+  return <StyledHeader>Header</StyledHeader>;
+}
+
+export default Header;
