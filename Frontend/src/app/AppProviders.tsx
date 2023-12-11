a322b372b19025cc8ee96cc47573dbd81aae5dec
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-12-11 22:38:34 +0200

    add i18n provider

--- Frontend/src/app/AppProviders.tsx
+++ Frontend/src/app/AppProviders.tsx
@@ -3,6 +3,8 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import { Toaster } from "react-hot-toast";
 import GlobalStyle from "./../styles/GlobalStyle.js";
+import { I18nextProvider } from "react-i18next";
+import i18n from "../i18n/i18n";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -10,36 +12,39 @@ const queryClient = new QueryClient({
     },
   },
 });
+
 type AppProvidersType = { children: React.ReactElement };
 
 function AppProviders({ children }: AppProvidersType) {
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <GlobalStyle />
-      <Toaster
-        position='top-center'
-        gutter={12}
-        containerStyle={{ margin: "8px" }}
-        toastOptions={
-          {
-            //   success: {
-            //     duration: 3000,
-            //   },
-            //   error: {
-            //     duration: 5000,
-            //   },
-            //   style: {
-            //     fontSize: "16px",
-            //     maxWidth: "500px",
-            //     padding: "16px 24px",
-            //     backgroundColor: "var(--color-grey-0)",
-            //     color: "var(--color-grey-700) ",
-            //   },
+      <I18nextProvider i18n={i18n}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <GlobalStyle />
+        <Toaster
+          position='top-center'
+          gutter={12}
+          containerStyle={{ margin: "8px" }}
+          toastOptions={
+            {
+              //   success: {
+              //     duration: 3000,
+              //   },
+              //   error: {
+              //     duration: 5000,
+              //   },
+              //   style: {
+              //     fontSize: "16px",
+              //     maxWidth: "500px",
+              //     padding: "16px 24px",
+              //     backgroundColor: "var(--color-grey-0)",
+              //     color: "var(--color-grey-700) ",
+              //   },
+            }
           }
-        }
-      />
-      {children}
+        />
+        {children}
+      </I18nextProvider>
     </QueryClientProvider>
   );
 }
