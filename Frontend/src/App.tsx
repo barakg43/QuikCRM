6ef0f611602e31963571025a9311fe1f771ba32a
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-12-11 22:36:56 +0200

    add pollen css to app

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import AppProviders from "./app/AppProviders";
 import AppRoutes from "./app/AppRoutes";
 import { BrowserRouter } from "react-router-dom";
+import "./styles/pollen.css";
+
 function App() {
   return (
     <AppProviders>
