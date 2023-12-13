c4539e60795a131ad04bd58e9262355ce211c362
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-12-13 08:13:42 +0200

    add some style to Header

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -3,7 +3,13 @@ import LanguageSelector from "../i18n/LanguageSelector";
 
 const StyledHeader = styled.header`
   display: flex;
-  font-size: var(--scale-00);
+  font-size: var(--scale-000);
+  background-color: var(--color-emerald-100);
+  padding: 0.5rem 1rem;
+  align-items: center;
+  gap: 2.4rem;
+  justify-content: flex-end;
+  border-bottom: 1px solid var(--color-grey-100);
 `;
 
 function Header() {
