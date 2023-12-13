3fd47738a18cee494eda5deaa659011a04da2681
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-12-13 08:12:31 +0200

    add genertic select component

--- Frontend/src/components/Select.tsx
+++ Frontend/src/components/Select.tsx
@@ -1,15 +1,21 @@
 import { ChangeEventHandler, Key } from "react";
 import styled from "styled-components";
 
-const StyledSelect = styled.select`
-  font-size: 1.4rem;
-  padding: 0.8rem 1.2rem;
-  border: 1px solid;
-`;
 export type Option = {
   value: string | number | readonly string[] | undefined;
   label: string | number | undefined;
 };
+
+const StyledSelect = styled.select<{ type: "white" | "dark" }>`
+  font-size: var(--scale-00);
+  padding: var(--size-1);
+  border: 1px solid
+    ${(props) =>
+      props.type === "white"
+        ? "var(--color-grey-100)"
+        : "var(--color-grey-300)"};
+`;
+
 type SelectProps = {
   options: Option[];
   value?: string | number | readonly string[] | undefined;
