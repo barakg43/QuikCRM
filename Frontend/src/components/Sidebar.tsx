50465e566c33363f44281b779abf5b9d605ed2ac
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-12-13 08:12:05 +0200

    fix sidebar transition

--- Frontend/src/components/Sidebar.tsx
+++ Frontend/src/components/Sidebar.tsx
@@ -9,15 +9,13 @@ const StyledSideBar = styled.aside`
   align-items: start;
   background-color: var(--color-emerald-500);
   border-right: 1px solid var(--color-emerald-200);
-  transition: all 300ms ease-in-out;
+  transition: width 300ms ease-in-out;
 
   width: var(--size-20);
   &:hover {
     width: var(--size-64);
   }
-  /* & span {
-    opacity: 100;
-  } */
+
   &:hover span {
     opacity: 100;
   }
