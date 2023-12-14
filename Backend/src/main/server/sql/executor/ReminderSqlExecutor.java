804ad5d2b5dd7862314de67e9db5083b09b3c4c6
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-12-15 00:53:09 +0200

    add reminders classes

--- Backend/src/main/server/sql/executor/ReminderSqlExecutor.java
+++ Backend/src/main/server/sql/executor/ReminderSqlExecutor.java
@@ -1,11 +1,34 @@
 package main.server.sql.executor;
 
+import main.server.http.json.dto.reminder.InvoiceReminderRecord;
+import main.server.http.json.dto.reminder.ReminderRecord;
+import main.server.http.json.dto.reminder.RenewReminderRecord;
 import main.server.sql.function.SqlFunctionExecutor;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
 public class ReminderSqlExecutor {
     private final SqlFunctionExecutor sqlFunctionExecutor;
 
     public ReminderSqlExecutor(SqlFunctionExecutor sqlFunctionExecutor) {
+        System.out.println("ReminderSqlExecutor ctor");
         this.sqlFunctionExecutor = sqlFunctionExecutor;
+    }
+
+    public List<RenewReminderRecord> getRenewsForTheNext2Month(){
+        return sqlFunctionExecutor.executeTableValueFunction("fncSystemsDetailsForDate", RenewReminderRecord.class, LocalDateTime.now());
+
+    }
+    public List<ReminderRecord> getReminders(){
+        return sqlFunctionExecutor.executeTableValueFunction("fncReminders", ReminderRecord.class, LocalDateTime.now());
+
+    }
+    public List<InvoiceReminderRecord> getInvoiceReminders()
+    {
+        return sqlFunctionExecutor.executeTableValueFunction("fncInvoicesForContracts_New", InvoiceReminderRecord.class, LocalDateTime.now());
+
     }
 }
