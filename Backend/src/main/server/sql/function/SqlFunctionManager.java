f075f3f70989729a39cd9351be0a603f8e9b1932
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-12-15 00:55:38 +0200

    deprecated  sql function manger

--- Backend/src/main/server/sql/function/SqlFunctionManager.java
+++ Backend/src/main/server/sql/function/SqlFunctionManager.java
@@ -29,23 +29,16 @@ public class SqlFunctionManager {
         this.sqlDatabaseConfig = sqlDatabaseConfig;
         this.functionExecutorMap = new HashMap<>();
         sqlClientLogger = LogManager.getLogger(ServerConstants.SQL_CLIENT_LOGGER_NAME);
-        sqlFunctionExecutor = new SqlFunctionExecutor(sqlDatabaseConfig);
+        sqlFunctionExecutor =null;
     }
 
 
-    public String getCostumerNameByID(int id) {
-        return sqlFunctionExecutor.executeScalarValueFunction(GET_CUSTOMER_SHORTNAME_BY_ID, id);
-    }
+//    public String getSupplierNameByID(int id) {
+//        return sqlFunctionExecutor.executeScalarValueFunction(GET_SUPPLIER_NAME_BY_ID, id);
+//    }
 
-
-    public String getSupplierNameByID(int id) {
-        return sqlFunctionExecutor.executeScalarValueFunction(GET_SUPPLIER_NAME_BY_ID, id);
-    }
-
-    public List<TaskRecord> getClosedTaskForCustomer(int id) {
-        return sqlFunctionExecutor.executeTableValueFunction(GET_CLOSED_TASK_FOR_CUSTOMER, id);
-
-    }
-
-
+//    public List<TaskRecord> getClosedTaskForCustomer(int id) {
+//        return sqlFunctionExecutor.executeTableValueFunction(GET_CLOSED_TASK_FOR_CUSTOMER, id);
+//
+//    }
 }
