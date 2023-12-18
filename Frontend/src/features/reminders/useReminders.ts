efe18c26cf00cb6be1da722e4569279165d3da13
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-12-18 21:58:22 +0200

    add reminder component

---
+++ Frontend/src/features/reminders/useReminders.ts
@@ -0,0 +1,14 @@
+import { QueryKey, useQuery } from "@tanstack/react-query";
+import { getAllReminders } from "../../services/apiReminders";
+
+export function useReminders() {
+  const {
+    isLoading,
+    data: reminders,
+    error,
+  } = useQuery({
+    queryKey: ["reminders"],
+    queryFn: getAllReminders,
+  });
+  return { reminders, error, isLoading };
+}
