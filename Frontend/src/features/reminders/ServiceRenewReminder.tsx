279a8c20c66c29cd5cd0540e1829d7abd2639c1c
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-12-20 17:09:35 +0200

    modify service reminder

--- Frontend/src/features/reminders/ServiceRenewReminder.tsx
+++ Frontend/src/features/reminders/ServiceRenewReminder.tsx
@@ -1,20 +1,24 @@
 import styled from "styled-components";
+import Heading from "../../components/Heading";
 
 // {
-//     "CustShortName": null,
-//     "SystemDetailDescription": null,
-//     "InternalIP": null,
-//     "ExternalIP": null,
-//     "UserName": null,
-//     "Password": null,
-//     "ValidityTill": null,
-//     "SystemDetailID": 0,
-//     "CustomerID": 0
-//   },
-const StyledReminderComponent = styled.div``;
+//   "dateOfReminder": "2023-11-16",
+//   "timeOfReminder": null,
+//   "reminderID": "713",
+//   "reminderRemark": "שחר-און שירותי ניהול בע\"מ: חוזה שירות-תאריך סיום-31/12/23",
+//   "responsibleUserName": "dbo",
+//   "closed": 0
+// }
+const StyledReminderComponent = styled.div`
+  display: flex;
+`;
 
 function ReminderComponent() {
-  return <StyledReminderComponent>ReminderComponent</StyledReminderComponent>;
+  return (
+    <StyledReminderComponent>
+      <Heading as='h3'></Heading>
+    </StyledReminderComponent>
+  );
 }
 
 export default ReminderComponent;
