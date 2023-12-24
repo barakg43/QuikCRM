46f111d0712658ed1f812cff7a586c9f231e84ad
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-12-24 18:19:10 +0200

    rename route from client to customer

--- Frontend/src/app/AppRoutes.tsx
+++ Frontend/src/app/AppRoutes.tsx
@@ -1,7 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Account from "../pages/Account.tsx";
 import Activities from "../pages/Activities";
-import Clients from "../pages/Clients";
+import Customers from "../pages/Customers.tsx";
 import PageNotFound from "../pages/PageNotFound";
 import Reminders from "../pages/Reminders";
 import Reports from "../pages/Reports";
@@ -14,10 +14,10 @@ function AppRoutes() {
   return (
     <Routes>
       <Route element={<AppLayout />}>
-        <Route index element={<Navigate replace to='/clients' />} />
+        <Route index element={<Navigate replace to='/customers' />} />
         <Route path='/reminders' element={<Reminders />} />
         <Route path='/activities' element={<Activities />} />
-        <Route path='/clients' element={<Clients />} />
+        <Route path='/customers' element={<Customers />} />
         <Route path='/reports' element={<Reports />} />
         <Route path='/settings' element={<Settings />} />
         <Route path='/account' element={<Account />} />
