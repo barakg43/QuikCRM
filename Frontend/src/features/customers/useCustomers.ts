22466fc3263fb38a051bd17dcc5cbb7f5a19e080
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-12-25 09:38:15 +0200

    add type to customers array

--- Frontend/src/features/customers/useCustomers.ts
+++ Frontend/src/features/customers/useCustomers.ts
@@ -1,12 +1,36 @@
-import { useQuery } from "@tanstack/react-query";
+import { UseQueryResult, useQuery } from "@tanstack/react-query";
 import { getAllCustomers } from "../../services/apiCustomers";
+import { CustomerStatus } from "./CustomersTable";
 
+type CustomerType = {
+  customerID: number;
+  activeContractID: number;
+  customerShortName: string;
+  customerName: string;
+  customerStatus: CustomerStatus;
+  customerIdentificationNumber: string;
+  customerMainPhone: string;
+  customerMainFax: string;
+  customerMainEMail: string;
+  customerWebSite: string;
+  remarks: string;
+  address: string;
+  city: string;
+  postalCode: string;
+  addressRemarks: string;
+  contactPersonName: string;
+  contactPersonPost: string;
+  contactPersonPhone: string;
+  contactPersonMobilePhone: string;
+  contactPersonFax: string;
+  contactPersonEMail: string;
+};
 export function useCustomers() {
   const {
     data: customers,
     isLoading,
     error,
-  } = useQuery({
+  }: UseQueryResult<CustomerType[]> = useQuery({
     queryKey: ["customers"],
     queryFn: getAllCustomers,
   });
