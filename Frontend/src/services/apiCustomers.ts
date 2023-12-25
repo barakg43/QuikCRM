b23750b216140468ef40c48acb81fb99579b7295
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-12-25 09:37:33 +0200

    fix customers request

--- Frontend/src/services/apiCustomers.ts
+++ Frontend/src/services/apiCustomers.ts
@@ -1,6 +1,6 @@
 import { httpClient } from "./axios";
 
 export async function getAllCustomers() {
-  const { data } = await httpClient.get("/reminders");
+  const { data } = await httpClient.get("/customers");
   return data;
 }
