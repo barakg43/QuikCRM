088442de083ad8cc4929df3587aaf9bb00f49f55
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-12-26 18:59:06 +0200

    fix scrolling outlet  in Applayout

--- Frontend/src/components/AppLayout.tsx
+++ Frontend/src/components/AppLayout.tsx
@@ -7,33 +7,69 @@ const StyledAppLayout = styled.div`
   display: flex;
   height: 100vh;
 `;
-const Main = styled.div`
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
   max-width: 120rem;
-  flex: 1 1 0%;
-  background-color: var(--color-primary-100);
+  /* flex: 1 1 0%; */
+  background-color: var(--color-primary-0);
 `;
-const Container = styled.main`
+const Container = styled.div`
   display: flex;
   justify-content: center;
-  flex: 1 1 0;
   max-width: 120rem;
+  gap: 3.2rem;
   margin: 0 auto;
   flex-direction: column;
-  padding: 1rem 1.5rem 0;
+`;
+const Main = styled.main`
+  background-color: var(--color-primary-100);
+  padding: 2rem;
+  overflow-y: scroll;
 `;
 
 function AppLayout() {
   return (
     <StyledAppLayout>
       <SideBar />
-      <Main>
+      <MainContainer>
         <Header />
-        <Container>
-          <Outlet />
-        </Container>
-      </Main>
+        <Main>
+          <Container>
+            <Outlet />
+          </Container>
+        </Main>
+      </MainContainer>
     </StyledAppLayout>
   );
 }
 
 export default AppLayout;
+{
+  /* <StyledAppLayout>
+<Header />
+<Main>
+  <Container>
+    <Outlet />
+  </Container>
+</Main>
+</StyledAppLayout> */
+}
+// const Main = styled.main`
+//   background-color: var(--color-grey-50);
+//   padding: 4rem 4.8rem 6.4rem;
+//   overflow: scroll;
+// `;
+// const StyledAppLayout = styled.main`
+//   display: grid;
+//   height: 100vh;
+//   grid-template-rows: auto 1fr;
+//   grid-template-columns: 26rem 1fr;
+// `;
+// const Container = styled.div`
+//   max-width: 120rem;
+//   margin: 0 auto;
+//   display: flex;
+//   flex-direction: column;
+//   gap: 3.2rem;
+// `;
