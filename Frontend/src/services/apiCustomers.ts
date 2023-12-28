02ad5c438c56a1fbf4aa1f13c79ddd9c9f44ef8a
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-12-28 15:59:49 +0200

    add support of pagination to getAllCustomers in apiCustomers

--- Frontend/src/services/apiCustomers.ts
+++ Frontend/src/services/apiCustomers.ts
@@ -1,6 +1,17 @@
+import { ITEMS_AMOUNT_PER_PAGE } from "../components/Pagination";
+import { CustomerDataType } from "../features/customers/customers";
 import { httpClient } from "./axios";
 
-export async function getAllCustomers() {
+type allCustomerParams = {
+  page: number;
+};
+
+export async function getAllCustomers({
+  page,
+}: allCustomerParams): Promise<CustomerDataType> {
+  const fromItem = (page - 1) * ITEMS_AMOUNT_PER_PAGE;
+  const toItem = fromItem + ITEMS_AMOUNT_PER_PAGE;
   const { data } = await httpClient.get("/customers");
-  return data;
+
+  return { customers: data.slice(fromItem, toItem), totalItems: data.length };
 }
