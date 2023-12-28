5bc92db7c7b33b4956e8af8c4eeab14b28d30a3a
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-12-28 15:57:36 +0200

    force app layout expend the all screen

--- Frontend/src/components/AppLayout.tsx
+++ Frontend/src/components/AppLayout.tsx
@@ -6,23 +6,28 @@ import { Outlet } from "react-router-dom";
 const StyledAppLayout = styled.div`
   display: flex;
   height: 100vh;
+  width: 100dvw;
 `;
 const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
-  /* flex: 1 1 0%; */
-  background-color: var(--color-primary-0);
+  flex: 1;
+
+  background-color: var(--color-primary-100);
 `;
 const Container = styled.div`
   display: flex;
+  flex-grow: 1 1 0;
+  /* height: 100vw; */
   justify-content: center;
   gap: 3.2rem;
   margin: 0 auto;
-  flex-direction: column;
+  flex-direction: row;
 `;
 const Main = styled.main`
   background-color: var(--color-primary-100);
   padding: 2rem;
+  height: 100vh;
   overflow-y: scroll;
 `;
 
