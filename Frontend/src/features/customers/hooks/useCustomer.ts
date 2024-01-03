9066d2ac6a8bed818b2b673b06d12c4b5e169b1b
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-01-03 20:58:30 +0200

    add useCustomer for customer api

--- Frontend/src/features/customers/hooks/useCustomer.ts
+++ Frontend/src/features/customers/hooks/useCustomer.ts
@@ -1,1 +1,15 @@
-export function useCustomer(customerId: number) {}
+import { useQuery } from "@tanstack/react-query";
+import { getCustomerDataByID } from "../../../services/apiCustomers";
+
+export function useCustomer(customerId: number) {
+  const {
+    data: customer,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["customer", customerId],
+    queryFn: () => getCustomerDataByID(customerId),
+  });
+
+  return { customer, isLoading, error };
+}
