51ffb3042f2487d5b508638bbd74b656e7e34c2c
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-01-11 13:13:46 +0200

    add BaseClause for update,select,insert,delete in sql

--- Backend/src/main/server/sql/bulider/component/BaseClause.java
+++ Backend/src/main/server/sql/bulider/component/BaseClause.java
@@ -1,2 +1,41 @@
-package main.server.sql.bulider.component;public enum BaseCluause {
+package main.server.sql.bulider.component;
+
+
+
+public enum BaseClause {
+//    SELECT @CustShortName = customerShortName
+//    FROM tbCustomers
+
+    SELECT("""
+            SELECT %s\s
+            FROM %%s\s
+             %%s\s"""),
+//    INSERT tbConnectionPriceQuotesDetails (
+////            PQSDetailsID,
+////            PQCDetailsID)
+////    VALUES (
+////            @PQSDetailsID,
+////            @PQCDetailsID)
+
+    INSERT("INSERT %%s \n %s"),
+
+//    UPDATE tbInvoicesForContracts
+//    SET dateOfDebit = DATEADD(MONTH, @MonthAmount, dateOfDebit)
+    UPDATE("UPDATE %%s \n %s"),
+    //    DELETE
+//    FROM tbConnectionPriceQuotesDetails
+    DELETE("DELETE %s\n FROM %%s ")
+    ;
+
+
+
+    final private String clauseString;
+
+    BaseClause(String clauseString){
+        this.clauseString=clauseString;
+    }
+
+    public String getClauseFormatString() {
+        return clauseString;
+    }
 }
