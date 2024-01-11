dd0ea29b4f78e6ec646ad12ead6de826dc6ae9b6
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-01-12 00:21:13 +0200

    some intend in query format

--- Backend/src/main/server/sql/bulider/component/SqlBaseClauses.java
+++ Backend/src/main/server/sql/bulider/component/SqlBaseClauses.java
@@ -3,40 +3,27 @@ package main.server.sql.bulider.component;
 import java.util.Map;
 
 public class SqlBaseClauses {
-    Map<String, Integer> map = Map.ofEntries(
-            Map.entry("A", 1),
-            Map.entry("B", 2),
-            Map.entry("C", 3),
-            Map.entry("D", 4),
-            Map.entry("E", 5),
-            Map.entry("F", 6),
-            Map.entry("G", 7),
-            Map.entry("H", 8),
-            Map.entry("I", 9),
-            Map.entry("J", 10),
-            Map.entry("K", 11),
-            Map.entry("L", 12)
-    );
-    protected BaseClause baseClause=null;
-    protected final StringBuilder additionalParameters=new StringBuilder();
-    private final String parameterDelimiter =", ";
-    private final SqlFilterClauses sqlFilterClauses;
+	protected final StringBuilder additionalParameters = new StringBuilder();
+	protected final StringBuilder additionalJoins = new StringBuilder();
+	private final String parameterDelimiter = ", ";
+	private final SqlFilterClauses sqlFilterClauses;
+	protected eBaseClause baseClause = null;
 
-    public SqlBaseClauses(SqlFilterClauses sqlFilterClauses) {
-        this.sqlFilterClauses = sqlFilterClauses;
-    }
 
-    public SqlFilterClauses select(String... columnNames) {
-        baseClauseDuplicateValidation();
-        baseClause = BaseClause.SELECT;
+	public SqlBaseClauses(SqlFilterClauses sqlFilterClauses) {
+		this.sqlFilterClauses = sqlFilterClauses;
+	}
 
-        for (String columnName:columnNames) {
-            additionalParameters.append(columnName).append(parameterDelimiter);
-        }
-        if(additionalParameters.length()>=2)
-            additionalParameters.delete(additionalParameters.length()-2,additionalParameters.length());
-        return sqlFilterClauses;
-    }
+	public SqlFilterClauses select(String... columnNames) {
+		baseClauseDuplicateValidation();
+		baseClause = eBaseClause.SELECT;
+
+		for (String columnName : columnNames) {
+			additionalParameters.append(columnName).append(parameterDelimiter);
+		}
+		removeLastDelimiterFromStringBuilder(additionalParameters);
+		return sqlFilterClauses;
+	}
 //    BEGIN TRANSACTION
 //    INSERT tbConnectionPriceQuotesDetails (
 //            PQSDetailsID,
@@ -53,26 +40,31 @@ public class SqlBaseClauses {
 //    COMMIT TRANSACTION
 
 
-    public SqlFilterClauses insert(Map<String,Object> columnValues){
-        baseClauseDuplicateValidation();
-        baseClause = BaseClause.INSERT;
-        StringBuilder columnNamesParameters=new StringBuilder("(");
-        StringBuilder valuesParameters=new StringBuilder("(");
-        for (Map.Entry<String,Object> columnEntry:columnValues.entrySet()) {
-            columnNamesParameters.append(columnEntry.getKey()).append(parameterDelimiter);
-            valuesParameters.append(columnEntry.getValue()).append(parameterDelimiter);
-        }
-        columnNamesParameters.append(")");
-        valuesParameters.append(")");
-        additionalParameters.append(columnNamesParameters).append(" VALUES ").append(valuesParameters);
-        return sqlFilterClauses;
-    }
-    /*
-    * BEGIN TRANSACTION
+	public SqlFilterClauses insert(Map<String, Object> columnValues) {
+		baseClauseDuplicateValidation();
+		baseClause = eBaseClause.INSERT;
+		StringBuilder columnNamesParameters = new StringBuilder();
+		StringBuilder valuesParameters = new StringBuilder();
+		for (Map.Entry<String, Object> columnEntry : columnValues.entrySet()) {
+			columnNamesParameters.append(columnEntry.getKey()).append(parameterDelimiter);
+			valuesParameters.append(columnEntry.getValue()).append(parameterDelimiter);
+		}
+		removeLastDelimiterFromStringBuilder(columnNamesParameters);
+		removeLastDelimiterFromStringBuilder(valuesParameters);
+		columnNamesParameters.append(")");
+		valuesParameters.append(")");
+
+		additionalParameters.append(columnNamesParameters).append("\n\t\t VALUES (").append(valuesParameters);
+		return sqlFilterClauses;
+	}
+
+
+	/*
+	* BEGIN TRANSACTION
 
 	UPDATE tbInvoicesForContracts
-    SET dateOfDebit = DATEADD(MONTH, @MonthAmount, dateOfDebit)
-    WHERE id = @ID
+	SET dateOfDebit = DATEADD(MONTH, @MonthAmount, dateOfDebit)
+	WHERE id = @ID
 
 		IF @@error <> 0
 			BEGIN
@@ -83,14 +75,15 @@ public class SqlBaseClauses {
 COMMIT TRANSACTION
 
 */
-    public SqlFilterClauses update(Map<String,Object> columnValues){
-        baseClauseDuplicateValidation();
-        baseClause = BaseClause.UPDATE;
-        for (Map.Entry<String,Object> columnEntry:columnValues.entrySet()) {
-            additionalParameters.append(columnEntry.getKey()).append(" = ").append(columnEntry.getValue()).append(parameterDelimiter);
-        }
-        return sqlFilterClauses;
-    }
+	public SqlFilterClauses update(Map<String, Object> columnValues) {
+		baseClauseDuplicateValidation();
+		baseClause = eBaseClause.UPDATE;
+		for (Map.Entry<String, Object> columnEntry : columnValues.entrySet()) {
+			additionalParameters.append(columnEntry.getKey()).append(" = ").append(columnEntry.getValue()).append(parameterDelimiter);
+		}
+		removeLastDelimiterFromStringBuilder(additionalParameters);
+		return sqlFilterClauses;
+	}
 //    BEGIN TRANSACTION
 //
 //    DELETE
@@ -106,15 +99,43 @@ COMMIT TRANSACTION
 //
 //    COMMIT TRANSACTION
 
-    public SqlFilterClauses delete(){
-        baseClauseDuplicateValidation();
-        baseClause = BaseClause.DELETE;
-        return sqlFilterClauses;
-    }
+	public SqlFilterClauses delete() {
+		baseClauseDuplicateValidation();
+		baseClause = eBaseClause.DELETE;
+		return sqlFilterClauses;
+	}
 
+	public SqlBaseClauses join(eJoinType joinType, String joinTableName) {
 
-    private  void baseClauseDuplicateValidation(){
-        if(baseClause!=null)
-            throw new RuntimeException("Cant select 2 clauses for same query");
-    }
+		return join(joinType, joinTableName, null, null, null);
+	}
+
+	public SqlBaseClauses join(eJoinType joinType, String joinTableName, String as) {
+
+		return join(joinType, joinTableName, as, null, null);
+	}
+
+	public SqlBaseClauses join(eJoinType joinType, String joinTableName, String as, String onColumnName1,
+							   String onColumnName2) {
+		additionalJoins.append(joinType).append(" ").append(joinTableName);
+		if (as != null) {
+			additionalJoins.append(" AS ").append(as);
+		}
+		if (onColumnName1 != null && onColumnName2 != null) {
+			additionalJoins.append(String.format(" ON %s = %s ", onColumnName1, onColumnName2));
+		}
+		return this;
+	}
+
+	//	JOIN dbo.tbCustomersAddresses AS ca ON cust.mainAddress = ca.customersAddressID
+//	JOIN dbo.tbCustomersContactPersons AS cp ON cust.mainContactPerson = cp.customersContactPersonID
+	private void removeLastDelimiterFromStringBuilder(StringBuilder clauseBuilder) {
+		if (clauseBuilder.length() >= parameterDelimiter.length())
+			clauseBuilder.delete(clauseBuilder.length() - parameterDelimiter.length(), clauseBuilder.length());
+	}
+
+	private void baseClauseDuplicateValidation() {
+		if (baseClause != null)
+			throw new RuntimeException("Cant select 2 clauses for same query");
+	}
 }
