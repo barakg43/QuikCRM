b4cf430b9f7ca061e54b1e10932c505d29771a92
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-01-12 00:19:40 +0200

    fix select format when using String.format

--- Backend/src/main/server/sql/bulider/component/SqlBaseClausesBuilder.java
+++ Backend/src/main/server/sql/bulider/component/SqlBaseClausesBuilder.java
@@ -2,34 +2,35 @@ package main.server.sql.bulider.component;
 
 public class SqlBaseClausesBuilder extends SqlBaseClauses {
 
-    public SqlBaseClausesBuilder(SqlFilterClauses sqlFilterClauses) {
-        super(sqlFilterClauses);
-    }
+	public SqlBaseClausesBuilder(SqlFilterClauses sqlFilterClauses) {
+		super(sqlFilterClauses);
+	}
 
-    public String formattedBaseStatementFormat(){
-        if(baseClause==null){
-            throw new RuntimeException("there are not selection for query clause");
-        }
-        String baseClauseWithoutFilter=String.format(
-                baseClause.getClauseFormatString(),additionalParameters);
+	public String formattedBaseStatementFormat() {
+		if (baseClause == null) {
+			throw new RuntimeException("there are not selection for query clause");
+		}
+
 //        System.out.println(baseClauseWithoutFilter);
-        if(baseClause.equals(BaseClause.SELECT)){
-            return baseClauseWithoutFilter;
-        }else{
-            String test="""
-                BEGIN TRANSACTION
-                       %s
-                       %%s
-                    IF @@error <> 0
-                    BEGIN
-                    ROLLBACK TRAN
-                    RETURN
-                            END
+		if (baseClause.equals(eBaseClause.SELECT)) {
+			return String.format(
+					baseClause.getClauseFormatString(), additionalParameters, additionalJoins);
+		} else {
+			String baseClauseWithoutFilter = String.format(
+					baseClause.getClauseFormatString(), additionalParameters);
+			//            System.out.println(test);
+			return """
+					BEGIN TRANSACTION
+					       %s\s
+					       %%s\s
+					    IF @@error <> 0
+					    BEGIN
+					    ROLLBACK TRAN
+					    RETURN
+					            END
 
-                COMMIT TRANSACTION""".formatted(baseClauseWithoutFilter);
-            System.out.println(test);
-            return test;
-        }
+					COMMIT TRANSACTION""".formatted(baseClauseWithoutFilter);
+		}
 
-    }
+	}
 }
---
+++ Backend/src/main/server/sql/bulider/component/eBaseClause.java
@@ -0,0 +1,53 @@
+package main.server.sql.bulider.component;
+
+
+public enum eBaseClause {
+//    SELECT @CustShortName = customerShortName
+//    FROM tbCustomers
+
+	SELECT("""
+			SELECT %s\s
+			FROM %%s %s\s
+			 %%s\s"""),
+
+//	SELECT("""
+//			SELECT %s\s
+//			FROM %%%s %%s\s
+//			 %%%s\s"""),
+
+//    INSERT tbConnectionPriceQuotesDetails (
+////            PQSDetailsID,
+////            PQCDetailsID)
+////    VALUES (
+////            @PQSDetailsID,
+////            @PQCDetailsID)
+
+	INSERT("""
+			INSERT %%s (\s
+			\t\t%s\s"""),
+
+
+	//    UPDATE tbInvoicesForContracts
+//    SET dateOfDebit = DATEADD(MONTH, @MonthAmount, dateOfDebit)
+	UPDATE("""
+			UPDATE %%s\s
+			\t\tSET %s\s
+			"""),
+	//    DELETE
+//    FROM tbConnectionPriceQuotesDetails
+	DELETE("""
+			DELETE %s\s
+			\t\tFROM %%s
+			""");
+
+
+	final private String clauseString;
+
+	eBaseClause(String clauseString) {
+		this.clauseString = clauseString;
+	}
+
+	public String getClauseFormatString() {
+		return clauseString;
+	}
+}
