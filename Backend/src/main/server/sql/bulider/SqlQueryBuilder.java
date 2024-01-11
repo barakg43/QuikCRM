de9174a697a5a4857740b4eec7fe8931ab50848b
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-01-11 13:14:57 +0200

    add build function for builders

--- Backend/src/main/server/sql/bulider/SqlQueryBuilder.java
+++ Backend/src/main/server/sql/bulider/SqlQueryBuilder.java
@@ -1,26 +1,40 @@
 package main.server.sql.bulider;
 
 import main.server.sql.bulider.component.SqlBaseClauses;
-import main.server.sql.bulider.component.SqlFilterClauses;
+import main.server.sql.bulider.component.SqlBaseClausesBuilder;
+import main.server.sql.bulider.component.SqlFilterClausesBuilder;
 
 public class SqlQueryBuilder {
 
-    private StringBuilder fromClause=null;
-    private SqlBaseClauses sqlBaseClauses=new SqlBaseClauses();
-    private SqlFilterClauses sqlFilterClauses=new SqlFilterClauses(this::buildQuery);
-    public SqlBaseClauses from(String tableName){
-        fromClause=new StringBuilder("FROM ");
-        fromClause.append(tableName);
-        return sqlBaseClauses;
-    }
 
-    //callable only from last step
-    private String buildQuery(){
-        if(fromClause==null)
-            throw new RuntimeException("Cannot build query without FROM clause");
-        StringBuilder queryBuilder=new StringBuilder();
+	private final SqlFilterClausesBuilder sqlFilterClausesBuilder;
+	private final SqlBaseClausesBuilder sqlBaseClausesBuilder;
+	private String tableName = null;
+
+	public SqlQueryBuilder() {
+		sqlFilterClausesBuilder = new SqlFilterClausesBuilder(this::buildQuery);
+		;
+		sqlBaseClausesBuilder = new SqlBaseClausesBuilder(sqlFilterClausesBuilder);
+	}
+
+	public SqlBaseClauses from(String tableName) {
+		this.tableName = tableName;
+		return sqlBaseClausesBuilder;
+	}
+
+	//callable only from last step
+	private String buildQuery() {
+		if (tableName == null)
+			throw new RuntimeException("Cannot build query without FROM clause");
+		StringBuilder queryBuilder = new StringBuilder();
+//		System.out.println(sqlBaseClausesBuilder.formattedBaseStatementFormat());
+		String baseClauseWithTableNameAndFilter = String.format(
+				sqlBaseClausesBuilder.formattedBaseStatementFormat(),
+				tableName, sqlFilterClausesBuilder.buildFilterClausePart());
+//        System.out.println(baseClauseWithTableNameAndFilter);
 
 
-    }
+		return baseClauseWithTableNameAndFilter;
+	}
 
 }
--- Backend/src/main/server/sql/bulider/component/SqlBaseClausesBuilder.java
+++ Backend/src/main/server/sql/bulider/component/SqlBaseClausesBuilder.java
@@ -1,2 +1,35 @@
-package main.server.sql.bulider.component;public class SqlBaseClausesBuilder {
+package main.server.sql.bulider.component;
+
+public class SqlBaseClausesBuilder extends SqlBaseClauses {
+
+    public SqlBaseClausesBuilder(SqlFilterClauses sqlFilterClauses) {
+        super(sqlFilterClauses);
+    }
+
+    public String formattedBaseStatementFormat(){
+        if(baseClause==null){
+            throw new RuntimeException("there are not selection for query clause");
+        }
+        String baseClauseWithoutFilter=String.format(
+                baseClause.getClauseFormatString(),additionalParameters);
+//        System.out.println(baseClauseWithoutFilter);
+        if(baseClause.equals(BaseClause.SELECT)){
+            return baseClauseWithoutFilter;
+        }else{
+            String test="""
+                BEGIN TRANSACTION
+                       %s
+                       %%s
+                    IF @@error <> 0
+                    BEGIN
+                    ROLLBACK TRAN
+                    RETURN
+                            END
+
+                COMMIT TRANSACTION""".formatted(baseClauseWithoutFilter);
+            System.out.println(test);
+            return test;
+        }
+
+    }
 }
