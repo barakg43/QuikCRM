7004549579af39a88b96d19f91c12e052bd88d9f
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-01-14 17:35:25 +0200

    some code format ,add customer endpoint by id

--- Backend/src/main/server/http/controlles/CustomerController.java
+++ Backend/src/main/server/http/controlles/CustomerController.java
@@ -2,13 +2,11 @@ package main.server.http.controlles;
 
 import main.server.http.HttpRequestExecutor;
 import main.server.sql.dto.TaskRecord;
+import main.server.sql.dto.customer.CustomerFlatDetailsRecord;
 import main.server.sql.dto.customer.CustomerFullDetailsRecord;
 import main.server.sql.executor.CustomerSqlExecutor;
 import org.springframework.http.HttpMethod;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
 import java.util.List;
@@ -17,47 +15,59 @@ import java.util.List;
 @RequestMapping("/api/customers")
 public class CustomerController {
 
-   private final HttpRequestExecutor httpRequestExecutor;
-    final private CustomerSqlExecutor customerSqlExecutor;
+	private final HttpRequestExecutor httpRequestExecutor;
+	final private CustomerSqlExecutor customerSqlExecutor;
 
-    public CustomerController(HttpRequestExecutor httpRequestExecutor, CustomerSqlExecutor customerSqlExecutor) {
-        this.httpRequestExecutor = httpRequestExecutor;
+	public CustomerController(HttpRequestExecutor httpRequestExecutor, CustomerSqlExecutor customerSqlExecutor) {
+		this.httpRequestExecutor = httpRequestExecutor;
 //        sqlClient = new SqlClient(SQL_YAML_CONFIG_LOCATION, logManager);
-        this.customerSqlExecutor = customerSqlExecutor;
-    }
+		this.customerSqlExecutor = customerSqlExecutor;
+	}
 
-    //    @PostConstruct //start sql connection after CustomerHttpService ctor
-    private void startSqlConnection() {
+	//    @PostConstruct //start sql connection after CustomerHttpService ctor
+	private void startSqlConnection() {
 //        sqlClient.createSqlConnection();
-    }
-    @GetMapping("/test")
-    public String getTest(){
-        System.out.println("test!!!");
-        return "test";
-    }
-    @GetMapping("")
-    public List<CustomerFullDetailsRecord> getAllCustomers() {
-        return httpRequestExecutor.executeHttpRequest(customerSqlExecutor::getAllCustomers,"/customers",HttpMethod.GET);
+	}
 
-    }
-    @GetMapping("/customer/name")
-    public String getCostumerNameByID(@RequestParam int id) {
-        return httpRequestExecutor.executeHttpRequest(()-> customerSqlExecutor.getCustomerNameByID(id),"/customer/name",HttpMethod.GET);
+	@GetMapping("/test")
+	public String getTest() {
+		System.out.println("test!!!");
+		return "test";
+	}
+
+	@GetMapping("")
+	public List<CustomerFlatDetailsRecord> getAllCustomers() {
+		return httpRequestExecutor.executeHttpRequest(customerSqlExecutor::getAllCustomers, "/customers",
+				HttpMethod.GET);
+
+	}
+
+	@GetMapping("/customer/name")
+	public String getCostumerNameByID(@RequestParam int id) {
+		return httpRequestExecutor.executeHttpRequest(() -> customerSqlExecutor.getCustomerNameByID(id), "/customer" +
+				"/name", HttpMethod.GET);
 //        return clientSqlExecutor.getClientNameByID(id);
-    }
+	}
 
-    @GetMapping("/customer/closed-tasks")
-    public List<TaskRecord> getClosedTaskForCustomer(@RequestParam int id) {
+	@GetMapping("{id}")
+	public CustomerFullDetailsRecord getFullCustomerDetailsForId(@PathVariable("id") int id) {
+		return httpRequestExecutor.executeHttpRequest(() -> customerSqlExecutor.getFullCustomerDetailsForId(id),
+				"/customer/" + id
+				, HttpMethod.GET);
+	}
+
+	@GetMapping("/customer/closed-tasks")
+	public List<TaskRecord> getClosedTaskForCustomer(@RequestParam int id) {
 //        return sqlClient.getClosedTaskForCustomer(id);
-        return null;
-    }
+		return null;
+	}
 
-    @GetMapping("/supplier/name")
-    public String getSupplierNameByID(@RequestParam int id) {
-        String result="";
-        Instant startTime = Instant.now();
+	@GetMapping("/supplier/name")
+	public String getSupplierNameByID(@RequestParam int id) {
+		String result = "";
+		Instant startTime = Instant.now();
 //        result = sqlClient.getSupplierNameByID(id);
-        Instant endTime = Instant.now();
-        return result;
-    }
+		Instant endTime = Instant.now();
+		return result;
+	}
 }
