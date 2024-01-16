b24b318c214f6f5be499b0bbcc2981650f1a9129
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-01-16 23:24:38 +0200

    change api server to spring-boot server insted json-server

--- Frontend/src/services/axios.ts
+++ Frontend/src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://barak-pc:9000";
-// const BASE_URL = "http://localhost:8080/api";
+// const BASE_URL = "http://barak-pc:9000";
+const BASE_URL = "http://localhost:8080/api";
 export const httpClient = axios.create({
   baseURL: BASE_URL,
 });
