61749f9ce11d7873d2c46fc787af549a96768846
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-01-19 12:17:44 +0200

    add customer object for CutomerForm

--- Frontend/src/features/customers/customer/CustomerForm.tsx
+++ Frontend/src/features/customers/customer/CustomerForm.tsx
@@ -1,7 +1,9 @@
 import { Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { CustomerFullDataType } from "../customers";
-
+import { useParams } from "react-router-dom";
+import { useCustomer } from "./useCustomer";
+import LoadingSpinner from "./../../../components/LoadingSpinner.tsx";
 // customerID,
 // customerShortName,
 // customerStatus,
@@ -26,13 +28,35 @@ function CustomerForm() {
   function onSubmit(data) {
     console.log(data);
   }
-  return (+62
+  const { customerId } = useParams();
+  // console.log("customerId", customerId);
+  const {
+    customer: {
+      customerID,
+      customerShortName,
+      customerStatus,
+      customerName,
+      customerIdentificationNumber,
+      customerMainPhone,
+      customerMainEMail,
+      remarks,
+      address,
+      city,
+      postalCode,
+      addressRemarks,
+      contactPersonName,
+      contactPersonMobilePhone,
+    } = {},
+    isLoading,
+  } = useCustomer(Number(customerId));
+  if (isLoading) return <LoadingSpinner />;
+  return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <FormControl isRequired>
         <FormLabel>First name</FormLabel>
         <Input placeholder='First name' />
       </FormControl>
-      s
+
       <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
         Save
       </Button>
