988108f0a6f2a922eb8c587bf79b8b9c3551c10d
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-01-27 21:22:13 +0200

    add loading spinner

--- Frontend/src/components/LoadingSpinner.tsx
+++ Frontend/src/components/LoadingSpinner.tsx
@@ -1,9 +1,30 @@
+import { Box, Container, Flex, Portal } from "@chakra-ui/react";
+import { createPortal } from "react-dom";
 import styled from "styled-components";
-
+import "./../styles/LoadingSpinner.css";
 const StyledLoadingSpinner = styled.div``;
 
 function LoadingSpinner() {
-  return <StyledLoadingSpinner>LoadingSpinner</StyledLoadingSpinner>;
+  return createPortal(<LoadingContainer />, document.body);
+}
+
+function LoadingContainer() {
+  return (
+    <Container
+      position='fixed'
+      centerContent
+      display='flex'
+      minHeight='100vh'
+      minW='100%'
+      alignItems='center'
+      justifyContent='center'
+      top={0}
+      left={0}
+      zIndex={1000}
+    >
+      <Box alignItems='stretch' className='bars-5'></Box>
+    </Container>
+  );
 }
 
 export default LoadingSpinner;
