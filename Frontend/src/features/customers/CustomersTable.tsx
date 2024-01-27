8f04659057196230b9707b470980b2f36c4fc06a
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-01-27 21:35:14 +0200

    make customer table expand the all parent div

--- Frontend/src/features/customers/CustomersTable.tsx
+++ Frontend/src/features/customers/CustomersTable.tsx
@@ -14,12 +14,11 @@ export const customerStatuses = [
 ] as const;
 
 function CustomersTable() {
-  const { customers, isLoading, totalItems, error } = useCustomers();
-
+  const { customers, isLoading, totalItems } = useCustomers();
   const { t } = useTranslation("customers", { keyPrefix: "table" });
-  if (isLoading) return <LoadingSpinner />;
+
   return (
-    <Table columns={"1fr ".repeat(5)}>
+    <Table columns={"1fr ".repeat(5)} minHeight='100%' minWeight='100%'>
       <Table.Header>
         <span>{t("customerId")}</span>
         <span>{t("customerName")}</span>
@@ -27,19 +26,24 @@ function CustomersTable() {
         <span>{t("city")}</span>
         <span>{t("status")}</span>
       </Table.Header>
-      <Table.Body
-        data={customers}
-        render={(customer) => (
-          <CustomerRow
-            customerID={customer.customerID}
-            customerShortName={customer.customerShortName}
-            address={customer.address}
-            city={customer.city}
-            customerStatus={customer.customerStatus}
-            key={customer.customerID}
-          />
-        )}
-      />
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <Table.Body
+          data={customers}
+          isLoading={isLoading}
+          render={(customer) => (
+            <CustomerRow
+              customerID={customer.customerID}
+              customerShortName={customer.customerShortName}
+              address={customer.address}
+              city={customer.city}
+              customerStatus={customer.customerStatus}
+              key={customer.customerID}
+            />
+          )}
+        />
+      )}
       <Table.Footer>
         {/* <Pagination totalItemsAmount={totalItems} /> */}
       </Table.Footer>
