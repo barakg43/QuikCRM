e136e58858d4ad02f945b586e07296745abe1df2
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-02-01 13:49:37 +0200

    refactor spaces in ReminderContoller

--- Backend/src/main/server/http/controlles/ReminderController.java
+++ Backend/src/main/server/http/controlles/ReminderController.java
@@ -3,7 +3,6 @@ package main.server.http.controlles;
 import main.server.http.HttpRequestExecutor;
 import main.server.sql.dto.reminder.InvoiceReminderRecord;
 import main.server.sql.dto.reminder.RenewReminderRecord;
-import main.server.sql.dto.reminder.ServiceRenewReminderRecord;
 import main.server.sql.executor.ReminderSqlExecutor;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -28,22 +27,17 @@ public class ReminderController {
 	@GetMapping("/renews")
 	public List<RenewReminderRecord> getRenews() {
 		return httpRequestExecutor.executeHttpRequest(reminderSqlExecutor::getRenews, "/api/reminders/renews",
-                HttpMethod.GET);
+				HttpMethod.GET);
 
 
 	}
 
-	@GetMapping("/service-renews")
-	public List<ServiceRenewReminderRecord> getServiceRenewReminders() {
-		return httpRequestExecutor.executeHttpRequest(reminderSqlExecutor::getServiceRenewReminders, "/api/reminders" +
-                "/service-renews", HttpMethod.GET);
 
-	}
-
-	@GetMapping("/invoices")
+	//	@GetMapping("/invoices")
+	@Deprecated
 	public List<InvoiceReminderRecord> getInvoiceReminders() {
 		return httpRequestExecutor.executeHttpRequest(reminderSqlExecutor::getInvoiceReminders, "/api/reminders" +
-                "/invoices", HttpMethod.GET);
+				"/invoices", HttpMethod.GET);
 	}
 
 }
