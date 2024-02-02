b15cb2b4ebed9a199bf4d83a3863c70251820d0e
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-02-02 16:30:28 +0200

    add test object for the componet Contract

--- Frontend/src/features/reminders/RemindersTable.tsx
+++ Frontend/src/features/reminders/RemindersTable.tsx
@@ -4,14 +4,24 @@ import { useReminders } from "./hooks/useReminders";
 import LoadingSpinner from "../../components/LoadingSpinner";
 
 const StyledRemindersTable = styled.div``;
-
+const test = {
+  ContractID: 15,
+  CustomerID: 38,
+  StartDateOfContract: Date.now(),
+  ContractPrice: 170,
+  FinishDateOfContract: Date.now(),
+  ReminderDate: Date.now(),
+  ContactDescription: " חידוש",
+  PeriodKind: "רבעון",
+  customerShortName: "אברהם",
+};
 function RemindersTable() {
   const { reminders, isLoading } = useReminders();
   if (isLoading) return <LoadingSpinner />;
   return (
-    <StyledRemindersTable>
+    <div>
       <InvoiceList />
-    </StyledRemindersTable>
+    </div>
   );
 }
 
