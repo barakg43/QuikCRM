daa8dc5d5840aec19368353c731cbaf26b9c1135
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-02-08 18:24:47 +0200

    add DetailsRow ro RenewContactReminder

--- Frontend/src/features/reminders/RenewContactReminder.tsx
+++ Frontend/src/features/reminders/RenewContactReminder.tsx
@@ -1,7 +1,19 @@
 import styled from "styled-components";
-import Heading from "../../components/Heading";
+import { Heading, Stack, StackDivider } from "@chakra-ui/react";
 import { ContactProps } from "./reminder";
-import { Card } from "@chakra-ui/react";
+import { Option } from "../../components/StyledSelect";
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  HStack,
+  Text,
+  chakra,
+} from "@chakra-ui/react";
+import { DetailRow } from "../../components/DetailRow";
 
 export type RenewContactProps = {
   ContractID: number;
@@ -26,8 +38,43 @@ function RenewContactReminder({
   PeriodKind,
 }: RenewContactProps) {
   return (
-    <Card>
-      <Heading as='h3'></Heading>
+    <Card _hover={{ backgroundColor: "teal.100" }}>
+      <CardHeader>
+        <Heading as='h4' fontSize='x-large' fontFamily='inherit'>
+          {ContractID} | {CustomerID}- {customerShortName}
+        </Heading>
+      </CardHeader>
+      <CardBody
+      // w={"max-content"}
+      // display='flex'
+      // flexDirection='column'
+      // gap='1rem'
+      >
+        <Stack divider={<StackDivider />} spacing='3'>
+          <HStack divider={<StackDivider />}>
+            <DetailRow
+              label={"StartDate"}
+              value={StartDateOfContract.toLocaleDateString()}
+            />
+            <DetailRow
+              label={"endDate"}
+              value={FinishDateOfContract.toLocaleDateString()}
+            />
+            <DetailRow label={"PeriodKind"} value={PeriodKind} />
+          </HStack>
+          <HStack>
+            <DetailRow
+              label={"ReminderDate"}
+              value={ReminderDate.toLocaleDateString()}
+            />
+            <DetailRow label={"ContractPrice"} value={ContractPrice} />
+          </HStack>
+          <DetailRow label={"ContactDescription"} value={ContactDescription} />
+        </Stack>
+      </CardBody>
+      <CardFooter>
+        <Button>Renew</Button>
+      </CardFooter>
     </Card>
   );
 }
