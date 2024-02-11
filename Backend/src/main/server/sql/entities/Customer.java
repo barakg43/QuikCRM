04b8a454c7b8cd0324baaf8b930d85d9fb9f7cc6
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-02-11 07:35:27 +0200

    add Customer entity

---
+++ Backend/src/main/server/sql/entities/Customer.java
@@ -0,0 +1,73 @@
+package main.server.sql.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+
+@Entity
+@Table(name = "tbCustomersDetails")
+@Getter
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private Short customerID;
+
+	@Column(nullable = false)
+	private String customerShortName;
+
+	@Column(nullable = false)
+	private String customerName;
+
+	@Column
+	private String customerIdentificationNumber;
+
+	@Column
+	private Byte customerStatusID;
+
+	@Column
+	private String customerMainPhone;
+
+	@Column
+	private String customerMainEMail;
+
+	@Lob
+	@Column
+	private String remarks;
+
+	@Column
+	private Long activeContractID;
+
+	@Column
+	private String address;
+
+	@Column
+	private String city;
+
+	@Column
+	private String postalCode;
+
+	@Lob
+	@Column
+	private String addressRemarks;
+
+	@Column
+	private String contactPersonName;
+
+	@Column
+	private String contactPersonPost;
+
+	@Column
+	private String contactPersonPhone;
+
+	@Column
+	private String contactPersonMobilePhone;
+
+//	@ManyToOne(fetch = FetchType.LAZY)
+//	@JoinColumn(name = "activeContractID", referencedColumnName = "ContractID", insertable = false, updatable = false)
+//	private CustomerContracts customerContracts;
+
+	// Constructors, getters, and setters
+}
