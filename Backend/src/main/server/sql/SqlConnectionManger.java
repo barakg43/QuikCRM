40995c46bae70ee3e5d6d428b25275e17c27d3fb
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-02-11 07:36:29 +0200

    create a beans in SqlConnectionManger

--- Backend/src/main/server/sql/SqlConnectionManger.java
+++ Backend/src/main/server/sql/SqlConnectionManger.java
@@ -3,7 +3,9 @@ package main.server.sql;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 
 import javax.sql.DataSource;
@@ -12,15 +14,19 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+import static main.server.ServerConstants.SQL_YAML_CONFIG_LOCATION;
+
+
+@Configuration
 public class SqlConnectionManger {
-	private final String sqlConfigurationYamlFilePath;
+	private final String sqlConfigurationYamlFilePath = SQL_YAML_CONFIG_LOCATION;
+
 	private DataSource dataSourceConfig;
 	private SqlConfiguration sqlConnectionConfiguration;
 	private Connection sqlConnection;
 	private SimpleJdbcCall simpleJdbcCall;
 
-	public SqlConnectionManger(String sqlConfigurationYamlFilePath) {
-		this.sqlConfigurationYamlFilePath = sqlConfigurationYamlFilePath;
+	public SqlConnectionManger() {
 	}
 
 	public void initializeSqlConnectionConfig() throws SQLException, IOException {
@@ -32,22 +38,21 @@ public class SqlConnectionManger {
 //        simpleJdbcCall.withFunctionName();
 	}
 
-	@Bean
-	private SqlConfiguration createSqlConfigurationFromFile(String filePath) throws IOException {
+	@Bean("sqlConfig")
+	public SqlConfiguration createSqlConfigurationFromFile() throws IOException {
 		ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory());
 		SqlConfiguration sqlConfiguration;
 		FileReader fileReader;
 
-		fileReader = new FileReader(filePath);
+		fileReader = new FileReader(sqlConfigurationYamlFilePath);
 		sqlConfiguration = yamlMapper.readValue(fileReader, SqlConfiguration.class);
 
 		return sqlConfiguration;
 	}
 
 	@Bean
-	private DataSource createDataSource(SqlConfiguration sqlServerConfiguration) {
+	public DataSource createDataSource(@Qualifier("sqlConfig") SqlConfiguration sqlServerConfiguration) {
 		SQLServerDataSource msSqlDataSource = new SQLServerDataSource();
-
 		msSqlDataSource.setServerName(sqlServerConfiguration.getServerName());
 		msSqlDataSource.setPortNumber(sqlServerConfiguration.getPort());
 		msSqlDataSource.setUser(sqlServerConfiguration.getUsername());
