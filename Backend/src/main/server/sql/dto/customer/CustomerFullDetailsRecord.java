86b4d2272b7af58f4c073e1c0be95e7a8e2ebbc1
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-02-12 12:39:57 +0200

    convert CustomerFullDetailsRecord to record

--- Backend/src/main/server/sql/dto/customer/CustomerFullDetailsRecord.java
+++ Backend/src/main/server/sql/dto/customer/CustomerFullDetailsRecord.java
@@ -1,32 +1,94 @@
 package main.server.sql.dto.customer;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Data;
+import main.server.sql.entities.eCustomerStatus;
 
-@Data
-@JsonIgnoreProperties(value = {"customerStatusID"})
-public class CustomerFullDetailsRecord {
 
-	private Integer customerID, activeContractID;
-	private String customerShortName;
-	private String customerName;
-	private String customerStatus;
-	private Integer customerStatusID;
-	private String customerIdentificationNumber;
-	private String customerMainPhone;
-	private String customerMainFax;
-	private String customerMainEMail;
-	private String customerWebSite;
-	private String remarks;
-	private String address;
-	private String city;
-	private String postalCode;
-	private String addressRemarks;
-	private String contactPersonName;
-	private String contactPersonPost;
-	private String contactPersonPhone;
-	private String contactPersonMobilePhone;
-	private String contactPersonFax;
-	private String contactPersonEMail;
+public record CustomerFullDetailsRecord(
+		Integer customerID,
+		Integer activeContractID,
+		String customerShortName,
+		String customerName,
+		eCustomerStatus customerStatus,
+		String customerIdentificationNumber,
+		String customerMainPhone,
+		String customerMainFax,
+		String customerMainEMail,
+		String customerWebSite,
+		String remarks,
+		String address,
+		String city,
+		String postalCode,
+		String addressRemarks,
+		String contactPersonName,
+		String contactPersonPost,
+		String contactPersonPhone,
+		String contactPersonMobilePhone,
+		String contactPersonFax,
+		String contactPersonEMail
+) {
+	public CustomerFullDetailsRecord(Integer customerID, Integer activeContractID, String customerShortName,
+									 String customerName, int customerStatusID,
+									 String customerIdentificationNumber, String customerMainPhone,
+									 String customerMainFax, String customerMainEMail, String customerWebSite,
+									 String remarks, String address, String city, String postalCode,
+									 String addressRemarks, String contactPersonName, String contactPersonPost,
+									 String contactPersonPhone, String contactPersonMobilePhone,
+									 String contactPersonFax, String contactPersonEMail) {
+		this(customerID,
+				activeContractID,
+				customerShortName,
+				customerName,
+				eCustomerStatus.values()[customerStatusID],
+				customerIdentificationNumber,
+				customerMainPhone,
+				customerMainFax,
+				customerMainEMail,
+				customerWebSite,
+				remarks,
+				address,
+				city,
+				postalCode,
+				addressRemarks,
+				contactPersonName,
+				contactPersonPost,
+				contactPersonPhone,
+				contactPersonMobilePhone,
+				contactPersonFax,
+				contactPersonEMail);
 
+	}
+
+	public CustomerFullDetailsRecord(Integer customerID, Integer activeContractID, String customerShortName,
+									 String customerName, eCustomerStatus customerStatus,
+									 String customerIdentificationNumber, String customerMainPhone,
+									 String customerMainFax, String customerMainEMail, String customerWebSite,
+									 String remarks, String address, String city, String postalCode,
+									 String addressRemarks, String contactPersonName, String contactPersonPost,
+									 String contactPersonPhone, String contactPersonMobilePhone,
+									 String contactPersonFax, String contactPersonEMail) {
+		this.customerID = customerID;
+		this.activeContractID = activeContractID;
+		this.customerShortName = customerShortName;
+		this.customerName = customerName;
+		this.customerStatus = customerStatus;
+		this.customerIdentificationNumber = customerIdentificationNumber;
+		this.customerMainPhone = customerMainPhone;
+		this.customerMainFax = customerMainFax;
+		this.customerMainEMail = customerMainEMail;
+		this.customerWebSite = customerWebSite;
+		this.remarks = remarks;
+		this.address = address;
+		this.city = city;
+		this.postalCode = postalCode;
+		this.addressRemarks = addressRemarks;
+		this.contactPersonName = contactPersonName;
+		this.contactPersonPost = contactPersonPost;
+		this.contactPersonPhone = contactPersonPhone;
+		this.contactPersonMobilePhone = contactPersonMobilePhone;
+		this.contactPersonFax = contactPersonFax;
+		this.contactPersonEMail = contactPersonEMail;
+	}
 }
+
+
+
