c66c791bb9e19324cc91728f7759fee859aad4e5
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-02-12 12:32:49 +0200

    add ProductReminder entity

---
+++ Backend/src/main/server/sql/entities/ProductReminder.java
@@ -0,0 +1,54 @@
+package main.server.sql.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Nationalized;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+
+@Setter
+@Getter
+@Entity
+@Table(name = "tbSystemsDetails")
+public class ProductReminder {
+	@Id
+	@Column(name = "SystemDetailID", nullable = false, precision = 18)
+	private BigDecimal id;
+
+	@Size(max = 50)
+	@Nationalized
+	@Column(name = "SystemDetailDescription", length = 50)
+	private String systemDetailDescription;
+
+	@Size(max = 50)
+	@Nationalized
+	@Column(name = "InternalIP", length = 50)
+	private String internalIP;
+
+	@Size(max = 50)
+	@Nationalized
+	@Column(name = "ExternalIP", length = 50)
+	private String externalIP;
+
+	@Size(max = 50)
+	@Nationalized
+	@Column(name = "UserName", length = 50)
+	private String userName;
+
+	@Size(max = 50)
+	@Nationalized
+	@Column(name = "Password", length = 50)
+	private String password;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "CustomerID", columnDefinition = "decimal", insertable = false, updatable = false)
+	private Customer customerID;
+
+	@Column(name = "ValidityTill")
+	private Instant validityTill;
+
+}
