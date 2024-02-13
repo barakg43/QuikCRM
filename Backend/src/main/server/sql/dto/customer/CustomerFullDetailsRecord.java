029bf7fc4ac504a0af6e1c54c6f85fb67635e8fe
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-02-13 16:49:45 +0200

    remove ctor from CustomerFullDetailsRecord

--- Backend/src/main/server/sql/dto/customer/CustomerFullDetailsRecord.java
+++ Backend/src/main/server/sql/dto/customer/CustomerFullDetailsRecord.java
@@ -1,7 +1,6 @@
 package main.server.sql.dto.customer;
 
 import main.server.sql.entities.eCustomerStatus;
-import org.springframework.data.annotation.PersistenceConstructor;
 
 
 public record CustomerFullDetailsRecord(
@@ -27,38 +26,6 @@ public record CustomerFullDetailsRecord(
 		String contactPersonFax,
 		String contactPersonEMail
 ) {
-	@PersistenceConstructor
-	public CustomerFullDetailsRecord(Integer customerID, Integer activeContractID, String customerShortName,
-									 String customerName, int customerStatusID,
-									 String customerIdentificationNumber, String customerMainPhone,
-									 String customerMainFax, String customerMainEMail, String customerWebSite,
-									 String remarks, String address, String city, String postalCode,
-									 String addressRemarks, String contactPersonName, String contactPersonPost,
-									 String contactPersonPhone, String contactPersonMobilePhone,
-									 String contactPersonFax, String contactPersonEMail) {
-		this(customerID,
-				activeContractID,
-				customerShortName,
-				customerName,
-				eCustomerStatus.values()[customerStatusID],
-				customerIdentificationNumber,
-				customerMainPhone,
-				customerMainFax,
-				customerMainEMail,
-				customerWebSite,
-				remarks,
-				address,
-				city,
-				postalCode,
-				addressRemarks,
-				contactPersonName,
-				contactPersonPost,
-				contactPersonPhone,
-				contactPersonMobilePhone,
-				contactPersonFax,
-				contactPersonEMail);
-
-	}
 
 }
 
