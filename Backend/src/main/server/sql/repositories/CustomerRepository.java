ea15c983b29556bb64682d1aa04c48ab2629f0ef
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-02-13 21:14:24 +0200

    add @Query to findAlCustomerDetails function

--- Backend/src/main/server/sql/repositories/CustomerRepository.java
+++ Backend/src/main/server/sql/repositories/CustomerRepository.java
@@ -11,10 +11,8 @@ import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-
-
-	//
-	@Query("SELECT c FROM Customer c")
+	@Query("SELECT new main.server.sql.dto.customer.CustomerSlimDetailsRecord(c.customerID, c.customerShortName, c" +
+			".customerStatus, c.customerMainPhone, c.address, c.city) FROM Customer c")
 	List<CustomerSlimDetailsRecord> findAlCustomerDetails();
 
 	CustomerFullDetailsRecord getCustomerByCustomerID(int customerID);
