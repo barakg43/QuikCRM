feb55ebc917eb5fb71db3d6ee7e82a067ccbfdec
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-02-14 00:14:30 +0200

    remove CustomerFullDetailsRecord ctor

--- Backend/src/main/server/sql/dto/customer/CustomerFullDetailsRecord.java
+++ Backend/src/main/server/sql/dto/customer/CustomerFullDetailsRecord.java
@@ -1,6 +1,5 @@
 package main.server.sql.dto.customer;
 
-import main.server.sql.entities.Customer;
 import main.server.sql.entities.eCustomerStatus;
 
 public record CustomerFullDetailsRecord(
@@ -22,26 +21,26 @@ public record CustomerFullDetailsRecord(
 		String contactPersonPhone,
 		String contactPersonMobilePhone
 ) {
-	public CustomerFullDetailsRecord(Customer customer) {
-		this(customer.getCustomerID(),
-				customer.getActiveContractID(),
-				customer.getCustomerShortName(),
-				customer.getCustomerName(),
-				customer.getCustomerStatus(),
-				customer.getCustomerIdentificationNumber(),
-				customer.getCustomerMainPhone(),
-				customer.getCustomerMainEMail(),
-				customer.getRemarks(),
-				customer.getAddress(),
-				customer.getCity(),
-				customer.getPostalCode(),
-				customer.getAddressRemarks(),
-				customer.getContactPersonName(),
-				customer.getContactPersonPost(),
-				customer.getContactPersonPhone(),
-				customer.getContactPersonMobilePhone());
-
-	}
+//	public CustomerFullDetailsRecord(Customer customer) {
+//		this(customer.getCustomerID(),
+//				customer.getActiveContractID(),
+//				customer.getCustomerShortName(),
+//				customer.getCustomerName(),
+//				customer.getCustomerStatus(),
+//				customer.getCustomerIdentificationNumber(),
+//				customer.getCustomerMainPhone(),
+//				customer.getCustomerMainEMail(),
+//				customer.getRemarks(),
+//				customer.getAddress(),
+//				customer.getCity(),
+//				customer.getPostalCode(),
+//				customer.getAddressRemarks(),
+//				customer.getContactPersonName(),
+//				customer.getContactPersonPost(),
+//				customer.getContactPersonPhone(),
+//				customer.getContactPersonMobilePhone());
+//
+//	}
 }
 
 
