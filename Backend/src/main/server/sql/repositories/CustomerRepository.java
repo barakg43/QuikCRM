3cec85a9ebd6670f0e90a401ce120adc11e5e957
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-02-13 16:48:52 +0200

    add findAlCustomerDetails,getCustomerByCustomerID to CustomerRepository

--- Backend/src/main/server/sql/repositories/CustomerRepository.java
+++ Backend/src/main/server/sql/repositories/CustomerRepository.java
@@ -1,15 +1,23 @@
 package main.server.sql.repositories;
 
+import main.server.sql.dto.customer.CustomerFullDetailsRecord;
+import main.server.sql.dto.customer.CustomerSlimDetailsRecord;
 import main.server.sql.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
 
-//
-//	CustomerFlatDetailsRecord findByCustomerID(Integer customerID);
+	//
+	@Query("SELECT c FROM Customer c")
+	List<CustomerSlimDetailsRecord> findAlCustomerDetails();
+
+	CustomerFullDetailsRecord getCustomerByCustomerID(int customerID);
 
 }
 
