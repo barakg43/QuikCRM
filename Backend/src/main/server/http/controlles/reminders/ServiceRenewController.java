4ce9e948057dcbeb977b3a36e13b129f161f0ca7
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-02-14 17:18:15 +0200

    fix ServiceContractService ref

--- Backend/src/main/server/http/controlles/reminders/ServiceRenewController.java
+++ Backend/src/main/server/http/controlles/reminders/ServiceRenewController.java
@@ -1,32 +1,28 @@
 package main.server.http.controlles.reminders;
 
 import main.server.http.HttpRequestExecutor;
-import main.server.sql.dto.reminder.ServiceRenewReminderRecord;
-import main.server.sql.services.ReminderService;
-import org.springframework.http.HttpMethod;
+import main.server.sql.services.ServiceContractService;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @CrossOrigin(origins = {"http://localhost:5173"})
 @RestController
 @RequestMapping("/api/reminders/service-renews")
 public class ServiceRenewController {
 	private final HttpRequestExecutor httpRequestExecutor;
-	private final ReminderService reminderService;
+	private final ServiceContractService serviceContractService;
 
-	public ServiceRenewController(HttpRequestExecutor httpRequestExecutor, ReminderService reminderService) {
+	public ServiceRenewController(HttpRequestExecutor httpRequestExecutor,
+								  ServiceContractService serviceContractService) {
 		this.httpRequestExecutor = httpRequestExecutor;
-		this.reminderService = reminderService;
+		this.serviceContractService = serviceContractService;
 	}
 
-	@GetMapping("")
-	public List<ServiceRenewReminderRecord> getServiceRenewReminders() {
-		return httpRequestExecutor.executeHttpRequest(reminderService::getServiceRenewReminders,
-				"/api/reminders/service-renews", HttpMethod.GET);
-
-	}
+//	@GetMapping("")
+//	public List<ServiceRenewReminderRecord> getServiceRenewReminders() {
+//		return httpRequestExecutor.executeHttpRequest(reminderService::getServiceRenewReminders,
+//				"/api/reminders/service-renews", HttpMethod.GET);
+//
+//	}
 }
