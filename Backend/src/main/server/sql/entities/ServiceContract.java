bc5721ab28599b30b77fbc9e8742329f4826a66e
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-02-14 17:12:05 +0200

    create ServiceContract entity

---
+++ Backend/src/main/server/sql/entities/ServiceContract.java
@@ -0,0 +1,68 @@
+package main.server.sql.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import main.server.sql.dto.reminder.ePeriodKind;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "tbCustomersContracts")
+@Getter
+@Setter
+public class ServiceContract {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(columnDefinition = "decimal")
+	private Long contractID;
+
+	@Column(nullable = false)
+	private Short customerID;
+
+	@Column
+	private Integer contactPersonID;
+
+	@Column(nullable = false)
+	private Byte contractsKindID;
+
+	@Column(nullable = false)
+	private Timestamp startDateOfContract;
+
+	@Column(nullable = false)
+	private Timestamp finishDateOfContract;
+
+	@Column(nullable = false)
+	private BigDecimal contractPrice;
+
+	@Column(nullable = false)
+	private Byte quantityOfMonths;
+
+	@Column(nullable = false)
+	private Float coefficient;
+
+	@Column
+	@Enumerated(EnumType.STRING)
+	private ePeriodKind periodKind;
+
+	@Column(nullable = false)
+	private Byte number;
+
+	@Column
+	private String contactDescription;
+
+	@Column
+	private Boolean renewed;
+
+	@Column
+	private Long reminderID;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "customerID", referencedColumnName = "customerID", insertable = false, updatable = false)
+	private Customer customer;
+
+	// Constructors, getters, and setters
+}
+
