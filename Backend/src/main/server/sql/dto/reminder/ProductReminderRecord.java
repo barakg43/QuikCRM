ee829b3987c0173903df23ea98176a57c4434d7b
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-02-15 20:55:33 +0200

    rename 'class RenewReminderRecord' to  'record ProductReminderRecord'

---
+++ Backend/src/main/server/sql/dto/reminder/ProductReminderRecord.java
@@ -0,0 +1,30 @@
+package main.server.sql.dto.reminder;
+
+import main.server.sql.entities.ProductReminderEntity;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+public record ProductReminderRecord(Short customerID,
+									String custShortName,
+									BigDecimal systemDetailID,
+									String systemDetailDescription,
+									String internalIP,
+									String externalIP,
+									String userName,
+									String password,
+									Timestamp validityTill) {
+	public static ProductReminderRecord convertFromEntityList(ProductReminderEntity productReminderEntity) {
+		return new ProductReminderRecord(productReminderEntity.getCustomer().getCustomerID(),
+				productReminderEntity.getCustomer().getCustomerShortName(),
+				productReminderEntity.getId(),
+				productReminderEntity.getSystemDetailDescription(),
+				productReminderEntity.getInternalIP(),
+				productReminderEntity.getExternalIP(),
+				productReminderEntity.getUserName(),
+				productReminderEntity.getPassword(),
+				productReminderEntity.getValidityTill());
+
+
+	}
+}
