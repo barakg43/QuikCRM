b58dd5e47007c7a6349026258eb730fa65273906
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-02-15 20:57:42 +0200

    create ProductRenewService class

--- Backend/src/main/server/sql/services/ProductRenewService.java
+++ Backend/src/main/server/sql/services/ProductRenewService.java
@@ -1,2 +1,28 @@
-package main.server.sql.services;public class ProductRenewService {
+package main.server.sql.services;
+
+
+import main.server.sql.dto.reminder.ProductReminderRecord;
+import main.server.sql.entities.ProductReminderEntity;
+import main.server.sql.repositories.ProductReminderRepository;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class ProductRenewService {
+
+	private final ProductReminderRepository productReminderRepository;
+
+
+	public ProductRenewService(ProductReminderRepository productReminderRepository) {
+		this.productReminderRepository = productReminderRepository;
+	}
+
+	public List<ProductReminderRecord> getRenewalReminders() {
+		List<ProductReminderEntity> productReminderEntityList =
+				productReminderRepository.findAllByValidityTillBefore(Timestamp.valueOf(LocalDateTime.now().plusMonths(2).toLocalDate().atStartOfDay()));
+		return productReminderEntityList.stream().map(ProductReminderRecord::convertFromEntityList).toList();
+	}
 }
