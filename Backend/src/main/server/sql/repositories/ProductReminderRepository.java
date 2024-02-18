58ebb522ea584152a8ab47701879933d16f38935
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-02-18 10:22:11 +0200

    fix ID in class def ProductReminderRepository,add findAllByCustomerID

--- Backend/src/main/server/sql/repositories/ProductReminderRepository.java
+++ Backend/src/main/server/sql/repositories/ProductReminderRepository.java
@@ -1,14 +1,16 @@
 package main.server.sql.repositories;
 
+import main.server.sql.dto.reminder.ProductReminderRecord;
 import main.server.sql.entities.ProductReminderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
-public interface ProductReminderRepository extends JpaRepository<ProductReminderEntity, Short> {
+public interface ProductReminderRepository extends JpaRepository<ProductReminderEntity, BigDecimal> {
 //	SELECT     TOP 100 PERCENT CustomerID, dbo.fncCustShortNameForCustID(CustomerID) AS CustShortName,
 ////	SystemDetailID, SystemDetailDescription,
 ////	InternalIP, ExternalIP, UserName, Password, ValidityTill
@@ -17,6 +19,9 @@ public interface ProductReminderRepository extends JpaRepository<ProductReminder
 ////	ORDER BY ValidityTill
 
 	List<ProductReminderEntity> findAllByValidityTillBefore(Timestamp validityTill);
+
+	List<ProductReminderRecord> findAllByCustomer_CustomerID(Short customerID);
+
 
 }
 
