a948c36c2fbb505b8501bf85eb225f92030e877a
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-02-18 19:45:21 +0200

    add CRUD endpoint to ServiceRenewController

--- Backend/src/main/server/http/controlles/reminders/ServiceRenewController.java
+++ Backend/src/main/server/http/controlles/reminders/ServiceRenewController.java
@@ -1,14 +1,16 @@
 package main.server.http.controlles.reminders;
 
 import main.server.http.HttpRequestExecutor;
+import main.server.sql.dto.reminder.ContractRecord;
 import main.server.sql.services.ContractService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = {"http://localhost:5173"})
 @RestController
-@RequestMapping("/api/reminders/service-renews")
+@RequestMapping("/api/reminders/service")
 public class ServiceRenewController {
 	private final HttpRequestExecutor httpRequestExecutor;
 	private final ContractService contractService;
@@ -19,10 +21,39 @@ public class ServiceRenewController {
 		this.contractService = contractService;
 	}
 
-//	@GetMapping("")
-//	public List<ServiceRenewReminderRecord> getServiceRenewReminders() {
-//		return httpRequestExecutor.executeHttpRequest(reminderService::getServiceRenewReminders,
-//				"/api/reminders/service-renews", HttpMethod.GET);
-//
-//	}
+	@GetMapping("/reminders")
+	public List<ContractRecord> getServiceRenewsReminders(int daysBeforeExpiration, int monthsAfterExpiration) {
+		return httpRequestExecutor.executeHttpRequest(() -> contractService.getServiceRenewReminders(monthsAfterExpiration, daysBeforeExpiration), "/api/reminders" +
+						"/renews",
+				HttpMethod.GET);
+
+
+	}
+
+
+	@PostMapping
+	public void addNewContract(@RequestBody ContractRecord contractRecord) {
+		httpRequestExecutor.executeHttpRequest(() -> contractService.addNewContract(contractRecord),
+				"/api/reminders/service", HttpMethod.POST);
+	}
+
+	@PatchMapping("/renew")
+	public void renewContractForPeriodTime(@RequestBody ContractRecord contractRecord) {
+		httpRequestExecutor.executeHttpRequest(() -> contractService.renewContractForPeriod(contractRecord),
+				"/api/reminders/service/renew", HttpMethod.PATCH);
+	}
+
+	@DeleteMapping
+	public void removeContract(@RequestParam Long contractId) {
+		httpRequestExecutor.executeHttpRequest(() -> contractService.deleteContractByID(contractId),
+				"/api/reminders/service", HttpMethod.DELETE);
+	}
+
+	@PatchMapping
+	public void updateContactData(@RequestBody ContractRecord contractRecord) {
+		httpRequestExecutor.executeHttpRequest(() -> contractService.updateContract(contractRecord),
+				"/api/reminders/service", HttpMethod.PATCH);
+	}
+
+
 }
