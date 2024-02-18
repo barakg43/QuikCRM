4a9999ed7158c2c57a4b55c48588d52cc0e2f848
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-02-18 10:45:42 +0200

    add renewProductForPeriodTime

--- Backend/src/main/server/sql/services/ProductRenewService.java
+++ Backend/src/main/server/sql/services/ProductRenewService.java
@@ -1,14 +1,19 @@
 package main.server.sql.services;
 
 
+import jakarta.persistence.EntityNotFoundException;
 import main.server.sql.dto.reminder.ProductReminderRecord;
+import main.server.sql.dto.reminder.ePeriodKind;
 import main.server.sql.entities.ProductReminderEntity;
 import main.server.sql.repositories.ProductReminderRepository;
+import main.server.uilities.UtilityFunctions;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductRenewService {
@@ -23,6 +28,27 @@ public class ProductRenewService {
 	public List<ProductReminderRecord> getRenewalReminders() {
 		List<ProductReminderEntity> productReminderEntityList =
 				productReminderRepository.findAllByValidityTillBefore(Timestamp.valueOf(LocalDateTime.now().plusMonths(2).toLocalDate().atStartOfDay()));
-		return productReminderEntityList.stream().map(ProductReminderRecord::convertFromEntityList).toList();
+		return productReminderEntityList.stream().map(ProductReminderRecord::convertFromEntity).toList();
+	}
+
+	public void renewProductForPeriodTime(BigDecimal reminderId, ePeriodKind periodKind) {
+		Optional<ProductReminderEntity> productReminderEntityToRenewOptional =
+				productReminderRepository.findById(reminderId);
+
+		if (productReminderEntityToRenewOptional.isPresent()) {
+			ProductReminderEntity currentProductReminder = productReminderEntityToRenewOptional.get();
+			ProductReminderEntity newReminder = new ProductReminderEntity();
+			newReminder.setCustomer(currentProductReminder.getCustomer());
+			newReminder.setPassword(currentProductReminder.getPassword());
+			newReminder.setExternalIP(currentProductReminder.getExternalIP());
+			newReminder.setSystemDetailDescription(currentProductReminder.getSystemDetailDescription());
+			newReminder.setInternalIP(currentProductReminder.getInternalIP());
+			newReminder.setUserName(currentProductReminder.getUserName());
+			newReminder.setValidityTill(UtilityFunctions.postDateByMonthAmount(currentProductReminder.getValidityTill(), periodKind));
+			currentProductReminder.setValidityTill(null);
+			productReminderRepository.save(currentProductReminder);
+			productReminderRepository.save(newReminder);
+		} else
+			throw new EntityNotFoundException();
 	}
 }
