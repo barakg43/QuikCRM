5b76fa69cc532b544bccdcb9c2f1cc7ec708a725
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-02-28 21:38:33 +0200

    migrate CutstomTable to use ChakraUI insted of  styled component

--- Frontend/src/components/CustomTable.tsx
+++ Frontend/src/components/CustomTable.tsx
@@ -1,39 +1,51 @@
-import { ResponsiveValue, Table } from "@chakra-ui/react";
+import {
+  ResponsiveValue,
+  Table,
+  Tbody,
+  Text,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import { MouseEventHandler, ReactNode, createContext, useContext } from "react";
 import styled from "styled-components";
 
 const TableContext = createContext<ValueType>({ columns: "" });
-const CommonRow = styled.div<{ columns: string }>`
-  display: grid;
-  font-size: 1.6rem;
-  grid-template-columns: ${(props) => props.columns};
-  column-gap: var(--scale-000);
-  align-items: center;
-`;
 
 const Footer = styled.footer``;
 
-const EmptyTable = styled.p`
-  font-size: 1.6rem;
-  font-weight: 500;
-  text-align: center;
-  margin: 2.4rem;
-`;
+// const EmptyTable = styled.p`
+//   font-size: 1.6rem;
+//   font-weight: 500;
+//   text-align: center;
+//   margin: 2.4rem;
+// `;
 
-const StyledBody = styled.section`
-  margin: 0.4rem 0;
-  min-height: 70vh;
-`;
 type BodyProps<T> = {
   data: T[] | null | undefined;
   render: (item: T, index: number) => JSX.Element;
   isLoading: boolean;
 };
+
+// const StyledBody = styled.tbody`
+//   margin: 0.4rem 0;
+//   min-height: 70vh;
+// `;
 function Body<T>({ data, render }: BodyProps<T>) {
   if (data == undefined || data == null || data.length == 0)
-    return <EmptyTable />;
+    return (
+      <Text
+        fontSize='1.2rem'
+        fontWeight={500}
+        textAlign='center'
+        margin='2.4rem'
+      />
+    );
 
-  return <StyledBody>{data.map(render)}</StyledBody>;
+  return (
+    <Tbody margin='0.4rem 0' minHeight='70vh'>
+      {data.map(render)}
+    </Tbody>
+  );
 }
 
 // const StyledTable = styled.div`
@@ -52,46 +64,80 @@ type TableProps = {
 function StyledTable({ columns, children, variant }: TableProps) {
   return (
     <TableContext.Provider value={{ columns }}>
-      <Table fontSize='xl' w='95%' paddingTop='' variant={variant}>
+      <Table
+        fontSize='xl'
+        w='95%'
+        paddingTop='var(--scale-3)'
+        variant={variant}
+      >
         {children}
       </Table>
     </TableContext.Provider>
   );
 }
 
-const StyledHeader = styled(CommonRow)`
-  padding: var(--scale-0);
-  background-color: var(--color-primary-300);
-  border-radius: var(--radius-md) var(--radius-md) 0 0;
-`;
+// const StyledHeader = styled(CommonRow)`
+//   padding: var(--scale-0);
+//   background-color: var(--color-primary-300);
+//   border-radius: var(--radius-md) var(--radius-md) 0 0;
+// `;
 type ValueType = {
   columns: string;
 };
 function Header({ children }: { children: ReactNode }) {
   const { columns } = useContext(TableContext);
   return (
-    <StyledHeader columns={columns} as='header'>
-      {children}
-    </StyledHeader>
+    <Thead>
+      <Tr
+        display='grid'
+        gridTemplateColumns={columns}
+        fontSize='1.6rem'
+        columnGap='var(--scale-000)'
+        alignItems='center'
+        textAlign='center'
+        padding='var(--scale-0)'
+        borderRadius='var(--radius-md) var(--radius-md) 0 0'
+        background='var(--color-primary-300)'
+      >
+        {children}
+      </Tr>
+    </Thead>
   );
 }
 
-const StyledRow = styled(CommonRow)`
-  padding: var(--scale-2) var(--scale-1);
-  &:not(:last-child) {
-    border-bottom: 1px var(--color-primary-300) solid;
-  }
-`;
 type RowType = {
   children: ReactNode;
   onClick: MouseEventHandler<HTMLDivElement> | undefined;
 };
+
+// const CommonRow = styled.div<{ columns: string }>`
+//   display: grid;
+//   font-size: 1.6rem;
+//   grid-template-columns: ${(props) => props.columns};
+//   column-gap: var(--scale-000);
+//   align-items: center;
+// `;
+// const StyledRow = styled(CommonRow)`
+//   padding: var(--scale-2) var(--scale-1);
+//   &:not(:last-child) {
+//     border-bottom: 1px var(--color-primary-300) solid;
+//   }
+// `;
 function Row({ onClick, children }: RowType) {
   const { columns } = useContext(TableContext);
   return (
-    <StyledRow columns={columns} role='row' onClick={onClick}>
+    <Tr
+      display='grid'
+      gridTemplateColumns={columns}
+      onClick={onClick}
+      fontSize='1.6rem'
+      columnGap='var(--scale-000)'
+      alignItems='center'
+      padding='var(--scale-2) var(--scale-1)'
+      _notLast={{ borderBottom: "1px var(--color-primary-300) solid" }}
+    >
       {children}
-    </StyledRow>
+    </Tr>
   );
 }
 
