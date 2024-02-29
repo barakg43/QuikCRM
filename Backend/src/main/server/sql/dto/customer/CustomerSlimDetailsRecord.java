1bf854c0f1345a233ea7b964dfd8fdbc25f9c0ae
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-02-29 20:44:04 +0200

    add CustomerSlimDetailsRecord ctor from  CustomerEntity

--- Backend/src/main/server/sql/dto/customer/CustomerSlimDetailsRecord.java
+++ Backend/src/main/server/sql/dto/customer/CustomerSlimDetailsRecord.java
@@ -1,8 +1,13 @@
 package main.server.sql.dto.customer;
 
+import main.server.sql.entities.CustomerEntity;
 import main.server.sql.entities.eCustomerStatus;
 
 public record CustomerSlimDetailsRecord(Short customerID, String customerShortName, eCustomerStatus customerStatus,
 										String customerMainPhone, String address, String city) {
 
+	public CustomerSlimDetailsRecord(CustomerEntity customerEntity) {
+		this(customerEntity.getCustomerID(), customerEntity.getCustomerShortName(), customerEntity.getCustomerStatus(),
+				customerEntity.getCustomerMainPhone(), customerEntity.getAddress(), customerEntity.getCity());
+	}
 }
