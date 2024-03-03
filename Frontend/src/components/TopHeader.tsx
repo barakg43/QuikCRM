649ef5e4973fa7a2baeee59b624d79dc4818cb1c
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-03-03 20:06:00 +0200

    replace styled-component in chakra ui component -TopHeader

--- Frontend/src/components/TopHeader.tsx
+++ Frontend/src/components/TopHeader.tsx
@@ -1,23 +1,22 @@
-import styled from "styled-components";
+import { Flex } from "@chakra-ui/react";
 import LanguageSelector from "../i18n/LanguageSelector";
-
-const StyledHeader = styled.header`
-  display: flex;
-  font-size: var(--scale-000);
-  background-color: var(--color-primary-100);
-  padding: 0.5rem 1rem;
-  align-items: center;
-  gap: 2.4rem;
-  justify-content: flex-end;
-  border-bottom: 2px solid var(--color-primary-300);
-  box-shadow: var(--shadow-xl);
-`;
 
 function TopHeader() {
   return (
-    <StyledHeader>
+    <Flex
+      as='header'
+      fontSize='x-large'
+      justifyContent='flex-end'
+      borderBottom='2px solid var(--color-primary-300)'
+      boxShadow='xl'
+      gap='2.4rem'
+      padding='0.5rem 1rem'
+      bgColor={"var(--color-primary-0)"}
+      height='5rem'
+      alignItems='center'
+    >
       <LanguageSelector />
-    </StyledHeader>
+    </Flex>
   );
 }
 
