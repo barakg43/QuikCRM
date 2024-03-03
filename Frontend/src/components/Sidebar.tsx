e9345b158de3921297f876774e686b557cd7a870
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-03-03 20:07:02 +0200

    replace styled-component in chakra ui component -Sidebar

--- Frontend/src/components/Sidebar.tsx
+++ Frontend/src/components/Sidebar.tsx
@@ -1,31 +1,18 @@
-import styled from "styled-components";
+import { VStack } from "@chakra-ui/react";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
-import { HStack, VStack } from "@chakra-ui/react";
-
-const StyledSideBar = styled.aside`
-  display: flex;
-  /* grid-row: 1/-1; */
-  /* grid-template-columns: auto; */
-  /* grid-template-rows: var(--size-44) 1fr; */
-  flex-direction: column;
-  /* align-items: start; */
-  gap: var(--scale-5);
-  color: var(--color-primary-100);
-  background-color: var(--color-primary-950);
-  border-right: 1px solid var(--color-primary-200);
-  transition: width 300ms ease-in-out;
-  overflow-x: clip;
-  width: 20rem;
-
-  &:hover span {
-    opacity: 100;
-  }
-`;
 
 function Sidebar() {
   return (
-    <VStack justifyContent='start' gap='10rem' w='20rem'>
+    <VStack
+      justifyContent='flex-start'
+      gap='5rem'
+      w='20rem'
+      as='nav'
+      borderInlineEnd={"2px solid var(--color-primary-300)"}
+      boxShadow='dark-lg'
+      // alignContent='start'
+    >
       <Logo />
       <MainNav />
     </VStack>
