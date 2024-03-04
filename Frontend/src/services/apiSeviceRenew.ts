db1078f0f6b27c84845f5ff3a2b7eaa1d813f662
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-03-04 20:26:05 +0200

    add apiServiceRenew for sever api operations

---
+++ Frontend/src/services/apiSeviceRenew.ts
@@ -0,0 +1,84 @@
+import { ServiceRenewRecord } from "../features/service-renews/service-renews";
+import { httpClient } from "./axios";
+
+export async function addNewServicesRenew({
+  customerID,
+  startDateOfContract,
+  contractPrice,
+  periodKind,
+  contactDescription,
+}: ServiceRenewRecord) {
+  try {
+    await httpClient.post(`/reminders/service`, {
+      customerID,
+      startDateOfContract,
+      contractPrice,
+      periodKind,
+      contactDescription,
+    });
+  } catch (error: unknown) {
+    console.log(error);
+  }
+}
+
+export async function updateServiceRenewDetails({
+  contractID,
+  startDateOfContract,
+  contractPrice,
+  periodKind,
+  contactDescription,
+}: ServiceRenewRecord) {
+  try {
+    httpClient.patch(`/reminders/service/${contractID}`, {
+      startDateOfContract,
+      contractPrice,
+      periodKind,
+      contactDescription,
+    });
+  } catch (error: unknown) {
+    console.log(error);
+  }
+}
+
+export async function getServiceRenewById(contractID: number) {}
+export async function renewService({
+  contractID,
+  contractPrice,
+  periodKind,
+  contactDescription,
+}: ServiceRenewRecord) {
+  try {
+    httpClient.patch(`/reminders/service/${contractID}/renew`, {
+      contractPrice,
+      periodKind,
+      contactDescription,
+    });
+  } catch (error: unknown) {
+    console.log(error);
+  }
+}
+export async function getAllServiceRenewForPeriodTime({
+  daysBeforeExpiration,
+  monthsAfterExpiration,
+}: {
+  daysBeforeExpiration: number;
+  monthsAfterExpiration: number;
+}) {
+  try {
+    httpClient.get(`/reminders/service/reminders`, {
+      params: {
+        daysBeforeExpiration,
+        monthsAfterExpiration,
+      },
+    });
+  } catch (error: unknown) {
+    console.log(error);
+  }
+}
+export async function deleteServiceRenew(contractID: number) {
+  try {
+    httpClient.delete(`/reminders/service/${contractID}`);
+  } catch (error: unknown) {
+    console.log(error);
+  }
+}
