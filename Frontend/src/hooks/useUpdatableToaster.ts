f9ebf998677706fea5b903f7e8e3e9d85980996d
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-03-11 13:10:00 +0200

    add useUpdatableToaster custom hook

---
+++ Frontend/src/hooks/useUpdatableToaster.ts
@@ -0,0 +1,34 @@
+import { Toast, ToastId, useToast } from "@chakra-ui/react";
+import { useRef } from "react";
+
+type statusOptions =
+  | "success"
+  | "info"
+  | "warning"
+  | "error"
+  | "loading"
+  | undefined;
+export function useUpdatableToaster(title: string) {
+  const toast = useToast();
+  const toastIdRef = useRef<ToastId>();
+
+  function updateToast(description: string, status: statusOptions) {
+    if (toastIdRef.current) toast.close(toastIdRef.current);
+    toast({
+      title,
+      description,
+      status,
+      duration: 3000,
+    });
+  }
+  function createInfinityToast(description: string, status: statusOptions) {
+    toastIdRef.current = toast({
+      title,
+      description,
+      status,
+      duration: null,
+    });
+  }
+
+  return { updateToast, createInfinityToast };
+}
