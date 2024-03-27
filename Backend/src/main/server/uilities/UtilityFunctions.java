fd8a6d301bd715b62567ffd79b78672dedce2392
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-03-27 07:51:56 +0200

    add validEntityValidations for check violations before saving to repo

--- Backend/src/main/server/uilities/UtilityFunctions.java
+++ Backend/src/main/server/uilities/UtilityFunctions.java
@@ -1,11 +1,17 @@
 package main.server.uilities;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import main.server.sql.dto.reminder.ePeriodKind;
 
 import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.util.Set;
 
 public class UtilityFunctions {
+	public static final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
 
 	public static Timestamp postDateByMonthAmount(Timestamp currentDate, ePeriodKind periodToAdd) {
 
@@ -35,5 +41,20 @@ public class UtilityFunctions {
 
 		return Timestamp.valueOf(localDate.plusDays(periodToAdd).atStartOfDay());
 
+	}
+
+	public static <T> void validEntityValidations(T entityObject) throws IllegalArgumentException {
+
+		Validator validator = factory.getValidator();
+		Set<ConstraintViolation<T>> violations = validator.validate(entityObject);
+		if (!violations.isEmpty()) {
+			StringBuilder stringBuilderViolations = new StringBuilder();
+
+			for (ConstraintViolation<T> violation : violations) {
+				stringBuilderViolations.append(violation.getPropertyPath()).append(':').append(violation.getMessage()).append('\n');
+			}
+			throw new IllegalArgumentException(stringBuilderViolations.toString());
+
+		}
 	}
 }
