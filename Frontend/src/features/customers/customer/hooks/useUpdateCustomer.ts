676dc89f42ed3d09f85dd8833976d9b7cb701d0d
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-03-28 17:16:45 +0200

    fix-use refetchQueries insted of invalidateQueries in useUpdateCustomer

--- Frontend/src/features/customers/customer/hooks/useUpdateCustomer.ts
+++ Frontend/src/features/customers/customer/hooks/useUpdateCustomer.ts
@@ -2,22 +2,21 @@ import { useToast } from "@chakra-ui/react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useTranslation } from "react-i18next";
 import { updateCustomerDetails_API } from "../../../../services/apiCustomers";
-import { useSearchParams } from "react-router-dom";
-export function useUpdateCustomer() {
+export function useUpdateCustomer(customerId: number) {
   const toast = useToast();
   const { t } = useTranslation("customers", { keyPrefix: "update" });
-  const [searchParams] = useSearchParams();
-  const customerId = Number(searchParams.get("customerId"));
   const queryClient = useQueryClient();
   const { mutate: updateCustomerDetails, isPending } = useMutation({
     mutationFn: updateCustomerDetails_API,
     onSuccess: () => {
+      console.log("success");
+      // queryClient.setQueryData(["user"], user);
       toast({
         description: t("toast-title"),
         title: t("toast-message-success"),
         status: "success",
       });
-      queryClient.invalidateQueries({
+      queryClient.refetchQueries({
         queryKey: ["customer", customerId],
       });
     },
