0253e5d7c6967948f6d786e7a395b9dd85c666b8
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-04-01 07:58:16 +0300

    add some style to Empty component

--- Frontend/src/components/Empty.tsx
+++ Frontend/src/components/Empty.tsx
@@ -1,8 +1,12 @@
+import { Text } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
 
 function Empty({ resource }: { resource: string }) {
   const { t } = useTranslation("components");
-
-  return <p>{t("empty", { resource })}</p>;
+  return (
+    <Text fontSize='1.2rem' fontWeight={500} textAlign='center' margin='2.4rem'>
+      {t("empty", { resource })}
+    </Text>
+  );
 }
 export default Empty;
