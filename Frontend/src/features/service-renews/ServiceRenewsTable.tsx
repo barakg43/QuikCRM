68be93413d76d853dc78f2c0b887bddc4a348cd6
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-04-01 07:53:58 +0300

    create service renew table

--- Frontend/src/features/service-renews/ServiceRenewsTable.tsx
+++ Frontend/src/features/service-renews/ServiceRenewsTable.tsx
@@ -1,39 +1,61 @@
+import { useTranslation } from "react-i18next";
+import { useServiceContractRenews } from "./hooks/useServiceContractRenews";
+import { Flex } from "@chakra-ui/react";
+import CustomTable from "./../../components/CustomTable.tsx";
+import TableHeaderCell from "../../components/TableHeaderCell";
+import LoadingSpinner from "../../components/LoadingSpinner";
+import ServiceRenewRow from "./ServiceRenewRow.tsx";
 function ServiceRenewsTable() {
-  return null;
-  // const { customers, isLoading, totalItems } = useCustomers();
-  // const { t } = useTranslation("customers", { keyPrefix: "table" });
-  // return (
-  //   <CustomTable columns={"1fr ".repeat(5)}>
-  //     <CustomTable.Header>
-  //       <TableHeaderCell label={t("customerId")} />
-  //       <TableHeaderCell label={t("customerName")} />
-  //       <TableHeaderCell label={t("address")} />
-  //       <TableHeaderCell label={t("city")} />
-  //       <TableHeaderCell label={t("status")} />
-  //     </CustomTable.Header>
-  //     {isLoading ? (
-  //       <LoadingSpinner />
-  //     ) : (
-  //       <CustomTable.Body
-  //         data={customers}
-  //         isLoading={isLoading}
-  //         render={(customer) => (
-  //           <CustomerRow
-  //             customerID={customer.customerID}
-  //             customerShortName={customer.customerShortName}
-  //             address={customer.address}
-  //             city={customer.city}
-  //             customerStatus={customer.customerStatus}
-  //             key={customer.customerID}
-  //           />
-  //         )}
-  //       />
-  //     )}
-  //     <CustomTable.Footer>
-  //       <Pagination totalItemsAmount={totalItems} />
-  //     </CustomTable.Footer>
-  //   </CustomTable>
-  // );
+  // return null;
+  const { serviceContractRenews, isLoading } = useServiceContractRenews();
+  const { t } = useTranslation("serviceRenews", { keyPrefix: "renew-table" });
+  console.log("serviceContractRenews", serviceContractRenews);
+  return (
+    <>
+      <Flex
+        alignContent='center'
+        justifyContent='flex-end'
+        paddingBottom='10px'
+        w='95%'
+      >
+        {/* <CustomerFormModal /> */}
+      </Flex>
+      <CustomTable columns={"1fr ".repeat(5)}>
+        <CustomTable.Header>
+          <TableHeaderCell label={t("customerShortName")} />
+          <TableHeaderCell label={t("startDateOfContract")} />
+          <TableHeaderCell label={t("finishDateOfContract")} />
+          <TableHeaderCell label={t("periodKind")} />
+        </CustomTable.Header>
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : (
+          <CustomTable.Body
+            data={serviceContractRenews}
+            isLoading={isLoading}
+            resourceName='title'
+            render={(serviceReminder) => (
+              <ServiceRenewRow
+                customerID={serviceReminder.customerID}
+                customerShortName={serviceReminder.customerShortName}
+                contractID={serviceReminder.contractID}
+                startDateOfContract={serviceReminder.startDateOfContract}
+                finishDateOfContract={serviceReminder.finishDateOfContract}
+                contractPrice={serviceReminder.contractPrice}
+                periodKind={serviceReminder.periodKind}
+                contactDescription={serviceReminder.contactDescription}
+                key={serviceReminder.contractID}
+              />
+            )}
+          />
+        )}
+        <CustomTable.Footer>
+          {""}
+          {/* <Pagination totalItemsAmount={totalItems} /> */}
+        </CustomTable.Footer>
+      </CustomTable>
+    </>
+  );
 }
 
 export default ServiceRenewsTable;
