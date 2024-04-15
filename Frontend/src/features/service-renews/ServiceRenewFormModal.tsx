53a4f92bff7da0c1e07c9864801c7f464ae6f903
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-04-15 18:35:58 +0300

    impl ServiceRenewForm on ModalBody

--- Frontend/src/features/service-renews/ServiceRenewFormModal.tsx
+++ Frontend/src/features/service-renews/ServiceRenewFormModal.tsx
@@ -11,60 +11,56 @@ import {
 } from "@chakra-ui/react";
 import { useRef } from "react";
 import { useTranslation } from "react-i18next";
-import CustomerForm from "./SeviceRenewForm";
+import ServiceRenewForm from "./ServiceRenewForm";
 import { ServiceRenewRecord } from "./serviceRenews";
 
 function ServiceRenewFormModal({
+  isOpen,
+  onClose,
   serviceRenew = {},
 }: {
+  isOpen: boolean;
+  onClose: () => void;
   serviceRenew?: ServiceRenewRecord | Record<string, never>;
 }) {
-  const { isOpen, onOpen, onClose } = useDisclosure();
   const { t } = useTranslation("serviceRenews", { keyPrefix: "renew-table" });
   const submitButtonRef = useRef<HTMLButtonElement>(null);
   return (
-    <>
-      <Button colorScheme='teal' onClick={onOpen} fontSize='1.2rem'>
-        {t("renew-button")}
-      </Button>
-      <Modal isCentered isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay
-          bg='blackAlpha.300'
-          backdropFilter='blur(10px) hue-rotate(90deg)'
-        />
-        <ModalContent minWidth={"50%"}>
-          <ModalHeader marginInlineStart='2rem' fontSize='1.6rem'>
-            {t("renew-title")}
-          </ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <CustomerForm
-              customerToEdit={customerToEdit}
-              submitRef={submitButtonRef}
-              OnSubmit={onClose}
-            />
-          </ModalBody>
-          <ModalFooter gap='1rem'>
-            <Button fontSize='1rem' onClick={onClose}>
-              {t("cancel-button")}
-            </Button>
-            <Button
-              fontSize='1.1rem'
-              fontWeight='bold'
-              onClick={() => {
-                submitButtonRef.current?.click();
+    <Modal isCentered isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay
+        bg='blackAlpha.300'
+        backdropFilter='blur(10px) hue-rotate(90deg)'
+      />
+      <ModalContent minWidth={"50%"}>
+        <ModalHeader marginInlineStart='2rem' fontSize='1.6rem'>
+          {t("renew-title")}
+        </ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <ServiceRenewForm
+            submitButtonRef={submitButtonRef}
+            serviceRenew={serviceRenew}
+            onSubmit={() => onClose()}
+          />
+        </ModalBody>
+        <ModalFooter gap='1rem'>
+          <Button fontSize='1rem' onClick={onClose}>
+            {t("cancel-button")}
+          </Button>
+          <Button
+            fontSize='1.1rem'
+            fontWeight='bold'
+            onClick={() => {
+              submitButtonRef.current?.click();
+              console.log("submit service");
+            }}
+          >
+            {t("renew-button")}
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
 
-                console.log("submit");
-              }}
-            >
-              {customerToEdit.customerID
-                ? t("update.button")
-                : t("add.save-button")}
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </>
     // </Suspense>
   );
 }
