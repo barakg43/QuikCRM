1a4df0028ddb1def6f6b98acd205fe3ca3c2f08a
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-04-21 18:38:28 +0300

    add @autowire to fields of StratupTests

--- Backend/test/java/com/quik/backend/StartupTests.java
+++ Backend/test/java/com/quik/backend/StartupTests.java
@@ -5,6 +5,7 @@ import main.server.http.controlles.GlobalEndpointsController;
 import main.server.http.controlles.reminders.ProductReminderController;
 import main.server.http.controlles.reminders.ServiceRenewController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,13 +13,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class StartupTests {
 
+	@Autowired
 	private GlobalEndpointsController globalEndpointsController;
+
+	@Autowired
 	private ProductReminderController productReminderController;
+
+	@Autowired
 	private CustomerController customerController;
+
+	@Autowired
 	private ServiceRenewController serviceRenewController;
 
 	@Test
-	void globalContextLoad() {
+	void globalContextLoad() throws Exception {
 		assertThat(globalEndpointsController).isNotNull();
 	}
 
