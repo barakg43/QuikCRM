d97419c0ed9f57d2ad7a11f5ce79add8fcbad98b
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-01 12:12:41 +0300

    use StyledModal in CustomerFormModal

--- Frontend/src/features/customers/customer/CustomerFormModal.tsx
+++ Frontend/src/features/customers/customer/CustomerFormModal.tsx
@@ -13,60 +13,36 @@ import { useRef } from "react";
 import { useTranslation } from "react-i18next";
 import { CustomerFullDataType } from "../customers";
 import CustomerForm from "./CustomerForm";
+import StyledModal from "../../../components/StyledModal";
 
 function CustomerFormModal({
   customerToEdit = {},
 }: {
   customerToEdit?: CustomerFullDataType | Record<string, never>;
 }) {
-  const { isOpen, onOpen, onClose } = useDisclosure();
   const { t } = useTranslation("customers");
-  const submitButtonRef = useRef<HTMLButtonElement>(null);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const isEditing = Object.keys(customerToEdit).length !== 0;
+
   return (
-    // <Suspense fallback={<LoadingSpinner />}>
     <>
       <Button colorScheme='teal' onClick={onOpen} fontSize='1.2rem'>
         {customerToEdit.customerID ? t("update.edit") : t("add.button")}
       </Button>
-      <Modal isCentered isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay
-          bg='blackAlpha.300'
-          backdropFilter='blur(10px) hue-rotate(90deg)'
-        />
-        <ModalContent minWidth={"50%"}>
-          <ModalHeader marginInlineStart='2rem' fontSize='1.6rem'>
-            {customerToEdit ? t("update.title") : t("add.title")}
-          </ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <CustomerForm
-              customerToEdit={customerToEdit}
-              submitRef={submitButtonRef}
-              OnSubmit={onClose}
-            />
-          </ModalBody>
-          <ModalFooter gap='1rem'>
-            <Button fontSize='1rem' onClick={onClose}>
-              {t("cancel-button")}
-            </Button>
-            <Button
-              fontSize='1.1rem'
-              fontWeight='bold'
-              onClick={() => {
-                submitButtonRef.current?.click();
-
-                console.log("submit");
-              }}
-            >
-              {customerToEdit.customerID
-                ? t("update.button")
-                : t("add.save-button")}
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+      <StyledModal
+        isOpen={isOpen}
+        isEditing={isEditing}
+        onClose={onClose}
+        title={isEditing ? t("update.title") : t("add.title")}
+        renderBody={(submitButtonRef) => (
+          <CustomerForm
+            customerToEdit={customerToEdit}
+            submitRef={submitButtonRef}
+            OnSubmit={onClose}
+          />
+        )}
+      />
     </>
-    // </Suspense>
   );
 }
 
