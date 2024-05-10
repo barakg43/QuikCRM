ae622b5520435aba7a4632e42bd65af2c3b6763f
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-10 09:01:01 +0300

    add ServiceRenewHistoryRow

---
+++ Frontend/src/features/customers/customer/child/service-renews/ServiceRenewHistoryRow.tsx
@@ -0,0 +1,54 @@
+import { Button, Td } from "@chakra-ui/react";
+import { useTranslation } from "react-i18next";
+import { useDeleteServiceContract } from "../../../../service-renews/hooks/useDeleteServiceContract";
+import { ServiceRenewRecord } from "../../../../service-renews/serviceRenews";
+import CustomTable from "../../../../../components/CustomTable";
+import { BodyTableCell } from "../../../BodyTableCell";
+
+type ServiceRenewHistoryProps = {
+  contractID: number;
+  startDateOfContract: Date;
+  finishDateOfContract: Date;
+  contractPrice: number;
+  contactDescription: string;
+};
+function ServiceRenewHistoryRow({
+  contractID,
+  contactDescription,
+  finishDateOfContract,
+  startDateOfContract,
+  contractPrice,
+}: ServiceRenewHistoryProps) {
+  const { t } = useTranslation("components", { keyPrefix: "buttons" });
+  const { deleteServiceContract, isPending } = useDeleteServiceContract();
+  function handleDelete() {
+    deleteServiceContract(contractID);
+  }
+  return (
+    <CustomTable.Row height='5.6rem'>
+      <BodyTableCell text={contractID} />
+      <BodyTableCell
+        text={new Date(startDateOfContract).toLocaleDateString("en-GB")}
+      />
+      <BodyTableCell
+        text={new Date(finishDateOfContract).toLocaleDateString("en-GB")}
+      />
+      <BodyTableCell text={contactDescription} />
+      <BodyTableCell text={contractPrice} />
+      <Td>
+        <Button
+          _focus={{ outline: "none", "box-shadow": "none" }}
+          isLoading={isPending}
+          _hover={{ backgroundColor: "teal.500", color: "white" }}
+          onClick={handleDelete}
+          fontSize={"xl"}
+          padding={"0.5rem 3rem 0.5rem 3rem"}
+        >
+          {t("delete")}
+        </Button>
+      </Td>
+    </CustomTable.Row>
+  );
+}
+
+export default ServiceRenewHistoryRow;
