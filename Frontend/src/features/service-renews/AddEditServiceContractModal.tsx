43f93adf584bf40ca8ddc9b1d0a71ce0079598bb
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-10 18:33:19 +0300

    fix props and variable name in AddEditServiceContractModal

--- Frontend/src/features/service-renews/AddEditServiceContractModal.tsx
+++ Frontend/src/features/service-renews/AddEditServiceContractModal.tsx
@@ -1,32 +1,22 @@
-import {
-  Button,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  useDisclosure,
-} from "@chakra-ui/react";
-import { useRef } from "react";
+import { Button, useDisclosure } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
-import { ServiceRenewRecord } from "./serviceRenews";
 import StyledModal from "../../components/StyledModal";
+import AddEditServiceContractForm from "./AddEditServiceContractForm";
+import { ServiceRenewRecord } from "./serviceRenews";
 
 function AddEditServiceContractModal({
-  customerToEdit = {},
+  serviceRenewToEdit = {},
 }: {
-  customerToEdit?: ServiceRenewRecord | Record<string, never>;
+  serviceRenewToEdit?: ServiceRenewRecord | Record<string, never>;
 }) {
   const { t } = useTranslation("customers");
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const isEditing = Object.keys(customerToEdit).length !== 0;
+  const isEditing = Object.keys(serviceRenewToEdit).length !== 0;
 
   return (
     <>
       <Button colorScheme='teal' onClick={onOpen} fontSize='1.2rem'>
-        {customerToEdit.customerID ? t("update.edit") : t("add.button")}
+        {serviceRenewToEdit.customerID ? t("update.edit") : t("add.button")}
       </Button>
       <StyledModal
         isOpen={isOpen}
@@ -35,9 +25,9 @@ function AddEditServiceContractModal({
         title={isEditing ? t("update.title") : t("add.title")}
         renderBody={(submitButtonRef) => (
           <AddEditServiceContractForm
-            customerToEdit={customerToEdit}
-            submitRef={submitButtonRef}
-            OnSubmit={onClose}
+            serviceRenewToEdit={serviceRenewToEdit}
+            submitButtonRef={submitButtonRef}
+            onSubmit={onClose}
           />
         )}
       />
