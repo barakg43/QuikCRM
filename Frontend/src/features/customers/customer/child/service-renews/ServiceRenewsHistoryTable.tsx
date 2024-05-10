78629ff09cae529dee0bf73d8545e36d1a1f2e9e
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-10 09:00:46 +0300

    add ServiceRenewsHistoryTable for customer details

---
+++ Frontend/src/features/customers/customer/child/service-renews/ServiceRenewsHistoryTable.tsx
@@ -0,0 +1,64 @@
+import { Box, Flex, useDisclosure } from "@chakra-ui/react";
+import { useTranslation } from "react-i18next";
+
+import CustomTable from "../../../../../components/CustomTable.tsx";
+import LoadingSpinner from "../../../../../components/LoadingSpinner.tsx";
+import { useServiceContractRenews } from "../../../../service-renews/hooks/useServiceContractRenews.ts";
+import { ServiceRenewRecord } from "../../../../service-renews/serviceRenews";
+import ServiceRenewFormModal from "./ServiceRenewFormModal.tsx";
+import ServiceRenewHistoryRow from "./ServiceRenewHistoryRow.tsx";
+function ServiceRenewsHistoryTable() {
+  const { serviceContractRenews, isLoading } = useServiceContractRenews();
+  const { t } = useTranslation("serviceRenews", { keyPrefix: "renew-table" });
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Flex
+        alignContent='center'
+        justifyContent='flex-end'
+        paddingBottom='10px'
+        w='95%'
+      >
+        <ServiceRenewFormModal
+          isOpen={isOpen}
+          onClose={onClose}
+          serviceRenew={{}}
+        />
+      </Flex>
+      <CustomTable columns={"1fr ".repeat(5)}>
+        <CustomTable.Header>
+          <CustomTable.Header.Cell label={t("contractID")} />
+          <CustomTable.Header.Cell label={t("startDateOfContract")} />
+          <CustomTable.Header.Cell label={t("finishDateOfContract")} />
+          <CustomTable.Header.Cell label={t("contactDescription")} />
+        </CustomTable.Header>
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : (
+          <CustomTable.Body
+            data={serviceContractRenews}
+            isLoading={isLoading}
+            resourceName={t("title")}
+            render={(serviceReminder: ServiceRenewRecord) => (
+              <ServiceRenewHistoryRow
+                contractID={serviceReminder.contractID}
+                startDateOfContract={serviceReminder.startDateOfContract}
+                finishDateOfContract={serviceReminder.finishDateOfContract}
+                contractPrice={serviceReminder.contractPrice}
+                contactDescription={serviceReminder.contactDescription}
+                key={serviceReminder.contractID}
+              />
+            )}
+          />
+        )}
+        <CustomTable.Footer>
+          <Box as='td'>{""}</Box>
+          {/* <Pagination totalItemsAmount={totalItems} /> */}
+        </CustomTable.Footer>
+      </CustomTable>
+    </>
+  );
+}
+
+export default ServiceRenewsHistoryTable;
