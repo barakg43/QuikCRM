7713786795a7f37045136fc7e45721847f5f2f59
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-13 08:49:44 +0300

    use grid in AddEditServiceContractForm insted of flex layout

--- Frontend/src/features/service-renews/AddEditServiceContractForm.tsx
+++ Frontend/src/features/service-renews/AddEditServiceContractForm.tsx
@@ -1,11 +1,16 @@
-import { Button, HStack, VStack } from "@chakra-ui/react";
-import { LegacyRef } from "react";
+import { Button, Grid } from "@chakra-ui/react";
+import { LegacyRef, useState } from "react";
 import { useForm } from "react-hook-form";
+import { DetailRow } from "../../components/DetailRow";
+import {
+  calculateForwardDateByMonthsAndDays,
+  getStringDate,
+} from "../../services/utils";
 import PeriodSelector from "./PeriodSelector";
 import ServiceFormRow from "./ServiceFormRow";
 import { useAddServiceContract } from "./hooks/useAddServiceContract";
 import { useUpdateServiceContract } from "./hooks/useUpdateServiceContract";
-import { ServiceRenewRecord } from "./serviceRenews";
+import { PeriodType, ServiceRenewRecord } from "./serviceRenews";
 
 function AddEditServiceContractForm({
   submitButtonRef,
@@ -20,15 +25,20 @@ function AddEditServiceContractForm({
     contactDescription,
     contractID,
     contractPrice,
-    finishDateOfContract,
     periodKind,
     startDateOfContract,
   } = serviceRenewToEdit;
+  const [period, setPeriod] = useState<PeriodType>(periodKind);
   const { addServiceContract } = useAddServiceContract();
   const { updateServiceContract } = useUpdateServiceContract();
   const { register, handleSubmit, formState, reset } =
     useForm<ServiceRenewRecord>();
   const { errors } = formState;
+  const periodToMonths = {
+    MONTHLY: 1,
+    QUARTERLY: 3,
+    YEARLY: 12,
+  };
   function onSubmitForm(data: ServiceRenewRecord) {
     if (serviceRenewToEdit) {
       updateServiceContract({ ...data, contractID });
@@ -38,42 +48,61 @@ function AddEditServiceContractForm({
     onSubmit?.();
     reset();
   }
-  const startDateDefaultValue = startDateOfContract
-    .toISOString()
-    .substring(0, 10);
+  const startDate = startDateOfContract ?? new Date();
+  const startDateDefaultValue = getStringDate(startDate);
+  const finishDateOfContract = getStringDate(
+    calculateForwardDateByMonthsAndDays({
+      startDate,
+      months: periodToMonths[period],
+    })
+  );
+
   return (
     <form onSubmit={handleSubmit(onSubmitForm)}>
-      <HStack gap={"1rem"} justifyContent='space-around'>
-        <VStack justifyItems='flex-start'>
-          <ServiceFormRow
-            label='startDateOfContract'
-            register={register}
-            defaultValue={startDateDefaultValue}
-            error={errors?.startDateOfContract}
-            type='date'
-          />
+      <Grid
+        gridTemplateAreas={`"start finish"
+                            "period period"
+                            "price contact"
+                            "child description"`}
+      >
+        <ServiceFormRow
+          label='startDateOfContract'
+          register={register}
+          defaultValue={startDateDefaultValue}
+          error={errors?.startDateOfContract}
+          type='date'
+          sx={{ gridArea: "start" }}
+        />
 
-          <ServiceFormRow
-            label='contractPrice'
-            register={register}
-            error={errors?.contractPrice}
-            defaultValue={contractPrice}
-            type='number'
-          />
-          <ServiceFormRow
-            label='contactDescription'
-            register={register}
-            error={errors?.contactDescription}
-            defaultValue={contactDescription}
-            type='textarea'
-          />
-          <PeriodSelector
-            defaultValue={periodKind}
-            register={register("periodKind", { required: true })}
-          />
-        </VStack>
-        <VStack></VStack>
-      </HStack>
+        <PeriodSelector
+          defaultValue={period}
+          onChange={setPeriod}
+          register={register("periodKind", { required: true })}
+          sx={{ gridArea: "period" }}
+        />
+        <DetailRow
+          label='finishDateOfContract'
+          value={finishDateOfContract}
+          sx={{ gridArea: "finish" }}
+        />
+
+        <ServiceFormRow
+          label='contractPrice'
+          register={register}
+          error={errors?.contractPrice}
+          defaultValue={contractPrice}
+          type='number'
+          sx={{ gridArea: "price" }}
+        />
+        <ServiceFormRow
+          label='contactDescription'
+          register={register}
+          error={errors?.contactDescription}
+          defaultValue={contactDescription}
+          type='textarea'
+          sx={{ gridArea: "description" }}
+        />
+      </Grid>
       <Button ref={submitButtonRef} display='none' type='submit' />
     </form>
   );
