1c66aba54e39a3fd82b0a546cc7727d70f9b9716
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-13 16:07:08 +0300

    use dd-MM-yyyy for getStringDate in utils

--- Frontend/src/services/utils.ts
+++ Frontend/src/services/utils.ts
@@ -13,5 +13,8 @@ export function calculateForwardDateByMonthsAndDays({
   return forwardDate;
 }
 export function getStringDate(date: Date) {
-  return date.toISOString().substring(0, 10);
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are zero based
+  const year = date.getFullYear();
+  return `${day}-${month}-${year}`;
 }
