254078b85efe42fd413491ba7719c0bb19fa86ef
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-13 08:45:32 +0300

    fix DEV_IP ip for backend server

--- Frontend/src/services/axios.ts
+++ Frontend/src/services/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const DEV_IP = "192.168.1.117";
+const DEV_IP = "barak-pc";
 const BASE_URL = import.meta.env.DEV
   ? `http://${DEV_IP}:8080/quik/api`
   : "http://10.10.40.20:8080/quik/api";
