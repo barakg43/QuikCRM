8c590905a7def67e821b30c1ca746f33283134df
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-14 11:41:15 +0300

    remove navigation to customer page on useDeleteCustomer

--- Frontend/src/features/customers/customer/hooks/useDeleteCustomer.ts
+++ Frontend/src/features/customers/customer/hooks/useDeleteCustomer.ts
@@ -1,21 +1,13 @@
 import { useToast } from "@chakra-ui/react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useTranslation } from "react-i18next";
-import { useNavigate, useSearchParams } from "react-router-dom";
 import { deleteCustomer_API } from "../../../../services/apiCustomers";
 export function useDeleteCustomer() {
   const toast = useToast();
-  //   const { createInfinityToast, updateToast } = useUpdatableToaster(
-  //     "delete service contract"
-  //   );
-  const [searchParams] = useSearchParams();
-  const customerId = Number(searchParams.get("customerId"));
-  const navigate = useNavigate();
   const { t } = useTranslation("customers", { keyPrefix: "delete" });
   const queryClient = useQueryClient();
   const { mutate: deleteCustomer, isPending } = useMutation({
-    mutationFn: () => deleteCustomer_API(customerId),
-    // onMutate: () => createInfinityToast("pending text", "loading"),
+    mutationFn: deleteCustomer_API,
     onSuccess: () => {
       toast({
         description: t("toast-title"),
@@ -25,7 +17,6 @@ export function useDeleteCustomer() {
       queryClient.invalidateQueries({
         queryKey: ["customers"],
       });
-      navigate("/customers");
     },
     onError: () =>
       toast({
