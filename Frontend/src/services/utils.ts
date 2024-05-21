4b51e7b300cab4a37d5b5e07b7d74eb14f680faa
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-21 08:24:18 +0300

    fix error date.getDay() not function in runtime

--- Frontend/src/services/utils.ts
+++ Frontend/src/services/utils.ts
@@ -13,8 +13,9 @@ export function calculateForwardDateByMonthsAndDays({
   return forwardDate;
 }
 export function getStringDate(date: Date) {
-  const day = String(date.getDate()).padStart(2, "0");
-  const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are zero based
-  const year = date.getFullYear();
+  const localDate = new Date(date);
+  const day = String(localDate.getDate()).padStart(2, "0");
+  const month = String(localDate.getMonth() + 1).padStart(2, "0"); // Months are zero based
+  const year = localDate.getFullYear();
   return `${day}-${month}-${year}`;
 }
