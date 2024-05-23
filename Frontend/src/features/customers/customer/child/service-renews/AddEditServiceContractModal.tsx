f17978cc1841e6e8e061b6000ab82fde614fdaab
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-23 10:56:38 +0300

    add icon for botton in AddEditServiceContractModal

--- Frontend/src/features/customers/customer/child/service-renews/AddEditServiceContractModal.tsx
+++ Frontend/src/features/customers/customer/child/service-renews/AddEditServiceContractModal.tsx
@@ -1,5 +1,6 @@
 import { Button, useDisclosure } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
+import { TbEdit, TbPencilCheck } from "react-icons/tb";
 import StyledModal from "../../../../../components/StyledModal";
 import { RenewServiceContractProps } from "../../../../service-renews/serviceRenews";
 import AddEditServiceContractForm from "./AddEditServiceContractForm";
@@ -9,14 +10,25 @@ function AddEditServiceContractModal({
 }: {
   serviceRenewToEdit?: RenewServiceContractProps | Record<string, never>;
 }) {
-  const { t } = useTranslation("customers");
+  const { t } = useTranslation("serviceRenews");
   const { isOpen, onOpen, onClose } = useDisclosure();
   const isEditing = Object.keys(serviceRenewToEdit).length !== 0;
 
   return (
     <>
-      <Button colorScheme='teal' onClick={onOpen} fontSize='1.2rem'>
-        {serviceRenewToEdit.contractID ? t("update.edit") : t("add.button")}
+      <Button
+        colorScheme='teal'
+        onClick={onOpen}
+        fontSize='1.3rem'
+        gap={2}
+        padding=' 0 1rem 0 1rem'
+      >
+        {serviceRenewToEdit.contractID ? (
+          <TbEdit size='5rem' />
+        ) : (
+          <TbPencilCheck />
+        )}
+        {serviceRenewToEdit.contractID ? t("update.button") : t("add.button")}
       </Button>
       <StyledModal
         isOpen={isOpen}
