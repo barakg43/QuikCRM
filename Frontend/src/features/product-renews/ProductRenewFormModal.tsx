a1f22d521da9bccf7869cc7f33aaf3dc8cd14a95
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-05-24 19:01:52 +0300

    create ProductRenewFormModal

---
+++ Frontend/src/features/product-renews/ProductRenewFormModal.tsx
@@ -0,0 +1,34 @@
+import { useTranslation } from "react-i18next";
+import StyledModal from "../../components/StyledModal";
+import ProductRenewForm from "./ProductRenewForm";
+
+function ProductRenewFormModal({
+  isOpen,
+  onClose,
+  productRenew = {},
+}: {
+  isOpen: boolean;
+  onClose: () => void;
+  productRenew?: ProductReminderRecord | Record<string, never>;
+}) {
+  const { t } = useTranslation("productRenews", { keyPrefix: "renew-table" });
+  const isEditing = Object.keys(productRenew).length !== 0;
+
+  return (
+    <StyledModal
+      isOpen={isOpen}
+      onClose={onClose}
+      isEditing={isEditing}
+      title={isEditing ? t("update.title") : t("add.title")}
+      renderBody={(submitButtonRef) => (
+        <ProductRenewForm
+          submitButtonRef={submitButtonRef}
+          productRenew={productRenew}
+          onSubmit={onClose}
+        />
+      )}
+    />
+  );
+}
+
+export default ProductRenewFormModal;
