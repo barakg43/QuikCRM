170cc284e47b0d6cfcf5d33a1c408d09ca2fcff0
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-06-14 17:27:19 +0300

    add config Base api url base on env -axios

--- Frontend/src/services/axios.ts
+++ Frontend/src/services/axios.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 const DEV_IP = "barak-pc";
-const BASE_URL = import.meta.env.DEV
+
+const isDevEnv = process.env.NODE_ENV === "development";
+const BASE_URL = isDevEnv
   ? `http://${DEV_IP}:8080/quik/api`
   : "http://10.10.40.20:8080/quik/api";
 // const BASE_URL = "http://barak-pc:8080/quik/api";
