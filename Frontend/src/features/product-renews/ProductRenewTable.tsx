f9ffb38a3859faab18ec61b42466f5d826e0ca79
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-06-14 17:42:27 +0300

    moving the renew modal to each row

--- Frontend/src/features/product-renews/ProductRenewTable.tsx
+++ Frontend/src/features/product-renews/ProductRenewTable.tsx
@@ -1,69 +1,35 @@
-import { Box, Flex, useDisclosure } from "@chakra-ui/react";
-import { useState } from "react";
+import { Box } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
 import CustomTable from "../../components/CustomTable";
-import ProductRenewFormModal from "./ProductRenewFormModal";
 import ProductRenewRow from "./ProductRenewRow";
 import { useProductRenews } from "./hooks/useProductRenews";
 
 function ProductRenewTable() {
   const { productRenews, isLoading } = useProductRenews();
-  const { t } = useTranslation("productRenews", { keyPrefix: "renew-table" });
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [productToRenew, setProductToRenew] = useState<ProductReminderRecord>();
-  function handleRenew(productRenew: ProductReminderRecord) {
-    setProductToRenew(productRenew);
-    onOpen();
-  }
-  return (
-    <>
-      <Flex
-        alignContent='center'
-        justifyContent='flex-end'
-        paddingBottom='10px'
-        w='95%'
-      >
-        <ProductRenewFormModal
-          isOpen={isOpen}
-          onClose={onClose}
-          productRenew={productToRenew}
-        />
-      </Flex>
-      <CustomTable columns={"1fr ".repeat(5)}>
-        <CustomTable.Header>
-          <CustomTable.Header.Cell label={t("systemDetailID")} />
-          <CustomTable.Header.Cell label={t("custShortName")} />
-          <CustomTable.Header.Cell label={t("productDetailDescription")} />
-          <CustomTable.Header.Cell label={t("validityTill")} />
-        </CustomTable.Header>
-        <CustomTable.Body
-          data={productRenews}
-          isLoading={isLoading}
-          resourceName={t("title")}
-          render={(productReminder) => (
-            <ProductRenewRow
-              systemDetailID={productReminder.systemDetailID}
-              productDetailDescription={
-                productReminder.productDetailDescription
-              }
-              custShortName={productReminder.custShortName}
-              customerID={productReminder.customerID}
-              notes1={productReminder.notes1}
-              notes2={productReminder.notes2}
-              notes3={productReminder.notes3}
-              notes4={productReminder.notes4}
-              validityTill={productReminder.validityTill}
-              onRenew={handleRenew}
-            />
-          )}
-        />
+  const { t } = useTranslation("productRenews");
 
-        <CustomTable.Footer>
-          <Box as='td'>{""}</Box>
-          {/* <Pagination totalItemsAmount={totalItems} /> */}
-        </CustomTable.Footer>
-      </CustomTable>
-    </>
+  return (
+    <CustomTable columns={"1fr ".repeat(5)}>
+      <CustomTable.Header>
+        <CustomTable.Header.Cell label={t("systemDetailID")} />
+        <CustomTable.Header.Cell label={t("custShortName")} />
+        <CustomTable.Header.Cell label={t("productDetailDescription")} />
+        <CustomTable.Header.Cell label={t("validityTill")} />
+      </CustomTable.Header>
+      <CustomTable.Body
+        data={productRenews}
+        isLoading={isLoading}
+        resourceName={t("title")}
+        render={(productReminder) => (
+          <ProductRenewRow productToRenew={productReminder} />
+        )}
+      />
+
+      <CustomTable.Footer>
+        <Box as='td'>{""}</Box>
+        {/* <Pagination totalItemsAmount={totalItems} /> */}
+      </CustomTable.Footer>
+    </CustomTable>
   );
 }
 
