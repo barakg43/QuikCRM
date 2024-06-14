2e8871fe1e32fc56d99289da29f8877f3c79eee8
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-06-14 17:46:24 +0300

    use Grid instend of Flex and VStack in ProductRenewPanel

--- Frontend/src/features/product-renews/ProductRenewPanel.tsx
+++ Frontend/src/features/product-renews/ProductRenewPanel.tsx
@@ -1,7 +1,8 @@
-import { Button, Flex, VStack } from "@chakra-ui/react";
+import { Button, Grid } from "@chakra-ui/react";
 import { LegacyRef } from "react";
 import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
+import ExtendFormRow from "../../components/ExtendFormRow";
 import { useRenewProductReminder } from "./hooks/useRenewProductReminder";
 
 export type ProductRenewPanelProps = {
@@ -54,61 +55,70 @@ function ProductRenewPanel({
 
   return (
     <form onSubmit={handleSubmit(onRenew)}>
-      <Flex grow={1} gap={6} fontSize='2xl'>
-        <VStack>
-          <ProductFormRow
-            label='productDetailDescription'
-            register={register}
-            error={errors?.productDetailDescription}
-            type='textarea'
-          />
-          <ProductFormRow
-            label='contractPrice'
-            register={register}
-            error={errors?.notes1}
-            defaultValue={notes1}
-            type='number'
-          />
-          <ProductFormRow
-            label='validityDate'
-            register={register}
-            error={errors?.validityDate}
-            defaultValue={notes1}
-            type='date'
-            isRequired
-          />
-        </VStack>
+      <Grid
+        gridTemplateRows={`1fr 1fr 1fr`}
+        gridTemplateColumns={`1fr 1fr`}
+        gap={6}
+        fontSize='2xl'
+      >
+        <ExtendFormRow
+          label='productDetailDescription'
+          register={register}
+          translationNS='productRenews'
+          error={errors?.productDetailDescription}
+          type='textarea'
+          defaultValue={productDetailDescription}
+          isRequired
+        />
 
-        <VStack>
-          <ProductFormRow
-            label='notes1'
-            register={register}
-            error={errors?.notes1}
-            type='textarea'
-          />
-          <ProductFormRow
-            label='contractPrice'
-            register={register}
-            error={errors?.notes1}
-            defaultValue={notes1}
-            type='number'
-          />
-          <ProductFormRow
-            label='validityDate'
-            register={register}
-            error={errors?.validityDate}
-            defaultValue={notes1}
-            type='date'
-            isRequired
-          />
-        </VStack>
+        <ExtendFormRow
+          label='validityTill'
+          register={register}
+          error={errors?.validityTill}
+          type='date'
+          isRequired
+          translationNS='productRenews'
+        />
+
+        <ExtendFormRow
+          label='notes1'
+          register={register}
+          defaultValue={notes1}
+          translationNS='productRenews'
+          error={errors?.notes1}
+          type='textarea'
+        />
+        <ExtendFormRow
+          label='notes2'
+          register={register}
+          defaultValue={notes2}
+          translationNS='productRenews'
+          error={errors?.notes2}
+          type='textarea'
+        />
+        <ExtendFormRow
+          label='notes3'
+          register={register}
+          defaultValue={notes3}
+          translationNS='productRenews'
+          error={errors?.notes3}
+          type='textarea'
+        />
+        <ExtendFormRow
+          label='notes4'
+          register={register}
+          defaultValue={notes4}
+          translationNS='productRenews'
+          error={errors?.notes4}
+          type='textarea'
+        />
         <Button
           ref={submitButtonRef}
           onClick={() => console.log("click submit")}
           display='none'
           type='submit'
         />
-      </Flex>
+      </Grid>
     </form>
   );
 }
