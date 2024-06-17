f4c6d9c8713731f7aa7e1d08b9b370e1bdcf03ef
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-06-17 15:24:17 +0300

    add interceptor for axios to return only the data

--- Frontend/src/services/axios.ts
+++ Frontend/src/services/axios.ts
@@ -8,4 +8,10 @@ const BASE_URL = isDevEnv
 // const BASE_URL = "http://barak-pc:8080/quik/api";
 export const httpClient = axios.create({
   baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+httpClient.interceptors.response.use(function (response) {
+  return response.data;
 });
