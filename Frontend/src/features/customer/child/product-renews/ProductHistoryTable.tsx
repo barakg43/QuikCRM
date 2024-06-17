aa7b15b773bc2c952b4ce6a81dda6f8a9043aaf4
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-06-17 19:18:38 +0300

    add ProductHistoryTable for customer in childTabs

---
+++ Frontend/src/features/customer/child/product-renews/ProductHistoryTable.tsx
@@ -0,0 +1,39 @@
+import { Box } from "@chakra-ui/react";
+import { useTranslation } from "react-i18next";
+import { useParams } from "react-router-dom";
+import CustomTable from "../../../../components/CustomTable";
+import { useProductRenewForCustomer } from "../../../product-renews/hooks/useProductRenewForCustomer";
+import ProductHistoryRow from "./ProductHistoryRow";
+
+function ProductHistoryTable() {
+  const { productRenews, isLoading } = useProductRenewForCustomer();
+  const { t } = useTranslation("productRenews");
+  console.log("productRenews", productRenews);
+  return (
+    <CustomTable columns={"1fr ".repeat(5)}>
+      <CustomTable.Header>
+        <CustomTable.Header.Cell label={t("systemDetailID")} />
+        <CustomTable.Header.Cell label={t("productDetailDescription")} />
+        <CustomTable.Header.Cell label={t("price")} />
+        <CustomTable.Header.Cell label={t("validityTill")} />
+      </CustomTable.Header>
+      <CustomTable.Body
+        data={productRenews}
+        isLoading={isLoading}
+        resourceName={t("title")}
+        render={(productReminder) => (
+          <ProductHistoryRow
+            productToRenew={productReminder}
+            key={productReminder.systemDetailID}
+          />
+        )}
+      />
+      <CustomTable.Footer>
+        <Box as='td'>{""}</Box>
+        {/* <Pagination totalItemsAmount={totalItems} /> */}
+      </CustomTable.Footer>
+    </CustomTable>
+  );
+}
+
+export default ProductHistoryTable;
