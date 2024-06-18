3f356d6292876304d72bbcd487ff09d3ab8f01ec
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-06-18 10:47:30 +0300

    fix useUpdateProductRenew function name

--- Frontend/src/features/product-renews/hooks/useUpdateProductRenew.ts
+++ Frontend/src/features/product-renews/hooks/useUpdateProductRenew.ts
@@ -2,7 +2,7 @@ import { useToast } from "@chakra-ui/react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useTranslation } from "react-i18next";
 import { updateProductReminder_API } from "../../../services/apiProductRenew";
-export function useAddServiceContract() {
+export function useUpdateProductRenew() {
   const toast = useToast();
   const { t } = useTranslation("productRenews", { keyPrefix: "update" });
   const queryClient = useQueryClient();
