84baf023aa7fb5883bc2e11555e839539a705f02
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-06-28 00:13:07 +0300

    add pagination to useProductRenews

--- Frontend/src/features/product-renews/hooks/useProductRenews.ts
+++ Frontend/src/features/product-renews/hooks/useProductRenews.ts
@@ -2,23 +2,32 @@ import { useToast } from "@chakra-ui/react";
 import { UseQueryResult, useQuery } from "@tanstack/react-query";
 import { useTranslation } from "react-i18next";
 import { useSearchParams } from "react-router-dom";
+import { usePageNumber } from "../../../hooks/usePageNumber";
 import { getAllProductReminderForPeriodTime_API } from "../../../services/apiProductRenew";
+import { SubsetListType } from "../../../services/globalTypes";
 
 export function useProductRenews() {
   const toast = useToast();
   const [searchParams] = useSearchParams();
   const { t } = useTranslation("productRenews", { keyPrefix: "renew-table" });
+  const page = usePageNumber();
   const daysBeforeExpiration =
     Number(searchParams.get("daysBeforeExpiration")) || 21;
   const {
-    data: productRenews = [],
+    data: { listSubset: productRenews, totalAmountInDataBase: totalItems } = {
+      listSubset: [],
+      totalAmountInDataBase: 0,
+    },
     isLoading,
     error,
-  }: UseQueryResult<ProductReminderRecord[] | never[]> = useQuery({
-    queryKey: ["product-renews", daysBeforeExpiration],
+  }: UseQueryResult<
+    SubsetListType<ProductReminderRecord> | never | undefined
+  > = useQuery({
+    queryKey: ["product-renews", daysBeforeExpiration, page],
     queryFn: () =>
       getAllProductReminderForPeriodTime_API({
         daysBeforeExpiration,
+        page,
       }),
   });
   if (error) {
@@ -31,5 +40,5 @@ export function useProductRenews() {
     });
   }
 
-  return { productRenews, isLoading };
+  return { productRenews, totalItems, isLoading };
 }
