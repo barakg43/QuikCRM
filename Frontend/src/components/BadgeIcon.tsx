14eb035af857cd8363f88f9624f86b77a78f0ffa
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-07-01 08:00:01 +0300

    fix BadgeIcon render error

--- Frontend/src/components/BadgeIcon.tsx
+++ Frontend/src/components/BadgeIcon.tsx
@@ -1,34 +1,35 @@
 import { Badge, Box } from "@chakra-ui/react";
+import { ReactElement } from "react";
 
 function BadgeIcon({
   icon,
   amountNotification = 0,
 }: {
-  icon: React.ReactNode;
+  icon: ReactElement | null | undefined;
   amountNotification?: number;
 }) {
-  return (
+  return amountNotification > 0 ? (
     <Box position='relative' justifyContent='center' alignContent='center'>
       {icon}
-      {amountNotification > 0 && (
-        <Badge
-          position='absolute'
-          top='0'
-          right='0'
-          borderRadius='full'
-          transform='translate(30%,0)'
-          padding='0.3rem'
-          display='grid'
-          placeItems='center'
-          bg='teal.300'
-          color='teal.900'
-          fontSize='0.8rem'
-          fontWeight='800'
-        >
-          {amountNotification}
-        </Badge>
-      )}
+      <Badge
+        position='absolute'
+        top='0'
+        right='0'
+        borderRadius='full'
+        transform='translate(30%,0)'
+        padding='0.3rem'
+        display='grid'
+        placeItems='center'
+        bg='teal.300'
+        color='teal.900'
+        fontSize='0.8rem'
+        fontWeight='800'
+      >
+        {amountNotification}
+      </Badge>
     </Box>
+  ) : (
+    <>{icon}</>
   );
 }
 
