043b24fa5f982a183b64407cb2edd0da1734a7e8
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-07-02 23:52:46 +0300

    handle null validity date and update button in Product tab in customer

--- Frontend/src/features/customer/child/product-renews/ProductHistoryRow.tsx
+++ Frontend/src/features/customer/child/product-renews/ProductHistoryRow.tsx
@@ -1,6 +1,7 @@
-import { Button, useDisclosure } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
 import CustomTable from "../../../../components/CustomTable";
+import { calculateForwardDateByMonthsAndDays } from "../../../../services/utils";
+import AddEditProductReminderModal from "./AddEditProductReminderModal";
 
 interface ProductHistoryRowProps {
   productToRenew: ProductReminderRecord;
@@ -9,31 +10,30 @@ function ProductHistoryRow({ productToRenew }: ProductHistoryRowProps) {
   const { productDetailDescription, systemDetailID, price, validityTill } =
     productToRenew;
   const { t } = useTranslation("productRenews", { keyPrefix: "renew-table" });
-  const { isOpen, onOpen, onClose } = useDisclosure();
   const Cell = CustomTable.Row.Cell;
+
+  const isFutureValidityDateHeadMonth =
+    validityTill &&
+    calculateForwardDateByMonthsAndDays({
+      startDate: validityTill,
+      months: 1,
+    }).getTime() >= Date.now();
   return (
-    <>
-      {/* <AddEditProductReminderModal productRenew={productToRenew} /> */}
-      <CustomTable.Row height='100%'>
-        <Cell sx={{ padding: 1 }}>{systemDetailID}</Cell>
-        <Cell sx={{ padding: 1 }}>{productDetailDescription}</Cell>
-        <Cell sx={{ padding: 1 }}>{price}</Cell>
-        <Cell sx={{ padding: 1 }}>
-          {new Date(validityTill).toLocaleDateString("en-GB")}
-        </Cell>
-        <Cell sx={{ padding: 1 }}>
-          <Button
-            _focus={{ outline: "none", boxShadow: "none" }}
-            _hover={{ backgroundColor: "teal.500", color: "white" }}
-            onClick={onOpen}
-            fontSize={"xl"}
-            padding={"0.5rem 3rem 0.5rem 3rem"}
-          >
-            {t("renew-button")}
-          </Button>
-        </Cell>
-      </CustomTable.Row>
-    </>
+    <CustomTable.Row height='100%'>
+      <Cell sx={{ padding: 1 }}>{systemDetailID}</Cell>
+      <Cell sx={{ padding: 1 }}>{productDetailDescription}</Cell>
+      <Cell sx={{ padding: 1 }}>{price}</Cell>
+      <Cell sx={{ padding: 1 }}>
+        {validityTill
+          ? new Date(validityTill).toLocaleDateString("en-GB")
+          : "-"}
+      </Cell>
+      <Cell sx={{ padding: 1 }}>
+        {isFutureValidityDateHeadMonth ? (
+          <AddEditProductReminderModal productRenewToEdit={productToRenew} />
+        ) : null}
+      </Cell>
+    </CustomTable.Row>
   );
 }
 
