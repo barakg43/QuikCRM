0f6536499b3f86a8f8f3c399c4af1ba1d05ea7fd
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-10-20 15:11:43 +0300

    fix null access when convertFromEntity  ProductReminderRecord in server

--- Backend/src/main/server/sql/dto/reminder/ProductReminderRecord.java
+++ Backend/src/main/server/sql/dto/reminder/ProductReminderRecord.java
@@ -1,9 +1,11 @@
 package main.server.sql.dto.reminder;
 
+import main.server.sql.entities.CustomerEntity;
 import main.server.sql.entities.ProductReminderEntity;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.util.Optional;
 
 public record ProductReminderRecord(Short customerID,
 									String custShortName,
@@ -16,8 +18,12 @@ public record ProductReminderRecord(Short customerID,
 									Integer price,
 									Timestamp validityTill) {
 	public static ProductReminderRecord convertFromEntity(ProductReminderEntity productReminderEntity) {
-		return new ProductReminderRecord(productReminderEntity.getCustomer().getCustomerID(),
-				productReminderEntity.getCustomer().getCustomerShortName(),
+		Optional<CustomerEntity> customerOptional = Optional.ofNullable(productReminderEntity.getCustomer());
+
+		Short customerID = customerOptional.map(CustomerEntity::getCustomerID).orElse(null);
+		String customerShortName = customerOptional.map(CustomerEntity::getCustomerShortName).orElse("");
+		return new ProductReminderRecord(customerID,
+				customerShortName,
 				productReminderEntity.getId(),
 				productReminderEntity.getProductDetailDescription(),
 				productReminderEntity.getNotes1(),
