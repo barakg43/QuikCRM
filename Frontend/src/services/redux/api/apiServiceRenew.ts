415d64420d9925435ca76726d8d1d03445398cc4
barakg43 <93736137+barakg43@users.noreply.github.com>
2024-10-21 09:30:39 +0300

    add serviceRenewApi base new RQT libaray

--- Frontend/src/services/redux/api/apiServiceRenew.ts
+++ Frontend/src/services/redux/api/apiServiceRenew.ts
@@ -4,6 +4,8 @@ import {
 } from "../../../features/service-renews/serviceRenews";
 import { httpClient } from "../../axios";
 import { ITEMS_AMOUNT_PER_PAGE, SubsetListType } from "../../globalTypes";
+import { translateToast } from "../../utils";
+import { baseApi } from "../baseApi";
 
 export async function addNewServicesRenew_API({
   customerID,
@@ -119,3 +121,165 @@ export async function deleteServiceRenew_API(contractID: number) {
     throw error;
   }
 }
+const serviceRenewApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    addServicesRenew: build.mutation<void, ServiceRenewRecord>({
+      query: (data) => ({
+        url: "/contract-service",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesKeys: () => ["contract-service"],
+      onSuccess: () => {
+        translateToast({
+          status: "success",
+          titleKey: "toast-message-success",
+          descriptionKey: "toast-title",
+          translationNS: "serviceRenews",
+          keyPrefix: "add",
+        });
+      },
+      onError: () => {
+        translateToast({
+          status: "error",
+          titleKey: "toast-message-error",
+          descriptionKey: "toast-title",
+          translationNS: "serviceRenews",
+          keyPrefix: "add",
+        });
+      },
+    }),
+    updateServiceRenew: build.mutation<void, ServiceRenewRecord>({
+      query: ({ contractID, ...data }) => ({
+        url: `/contract-service/${contractID}`,
+        method: "PATCH",
+        body: data,
+      }),
+      invalidatesKeys: () => ["contract-service"],
+      onSuccess: () => {
+        translateToast({
+          status: "success",
+          titleKey: "toast-message-success",
+          descriptionKey: "toast-title",
+          translationNS: "serviceRenews",
+          keyPrefix: "update",
+        });
+      },
+      onError: () => {
+        translateToast({
+          status: "error",
+          titleKey: "toast-message-error",
+          descriptionKey: "toast-title",
+          translationNS: "serviceRenews",
+          keyPrefix: "update",
+        });
+      },
+    }),
+    renewServiceReminder: build.mutation<void, RenewServiceContract>({
+      query: ({ contractID, ...data }) => ({
+        url: `/contract-service/${contractID}/renew`,
+        method: "PATCH",
+        body: data,
+      }),
+      invalidatesKeys: () => ["contract-service"],
+      onSuccess: () => {
+        translateToast({
+          status: "success",
+          titleKey: "toast-message-success",
+          descriptionKey: "toast-title",
+          translationNS: "serviceRenews",
+          keyPrefix: "renew",
+        });
+      },
+      onError: () => {
+        translateToast({
+          status: "error",
+          titleKey: "toast-message-error",
+          descriptionKey: "toast-title",
+          translationNS: "serviceRenews",
+          keyPrefix: "renew",
+        });
+      },
+    }),
+    serviceRenewForPeriodTime: build.query({
+      query: ({
+        daysBeforeExpiration,
+        monthsAfterExpiration,
+        page,
+      }: {
+        daysBeforeExpiration: number;
+        monthsAfterExpiration: number;
+        page: number;
+      }) => ({
+        url: "/contract-service/reminders",
+        params: {
+          daysBeforeExpiration,
+          monthsAfterExpiration,
+          pageNumber: page,
+          pageSize: ITEMS_AMOUNT_PER_PAGE,
+        },
+      }),
+      transformResponse: (data: SubsetListType<ServiceRenewRecord>) => ({
+        serviceContractRenews: data.listSubset,
+        totalItems: data.totalAmountInDataBase,
+      }),
+      providesQueryKeys: () => ["contract-service"],
+    }),
+    serviceContractHistoryCustomer: build.query({
+      query: ({
+        customerId,
+        page,
+        itemsPerPage,
+      }: {
+        customerId: number;
+        page: number;
+        itemsPerPage: number;
+      }) => ({
+        url: `/contract-service/customer/${customerId}`,
+        params: {
+          pageNumber: page,
+          pageSize: itemsPerPage,
+        },
+      }),
+      transformResponse: (data: SubsetListType<ServiceRenewRecord>) => ({
+        serviceContractRenews: data.listSubset,
+        totalItems: data.totalAmountInDataBase,
+      }),
+
+      providesQueryKeys: () => ["contract-service"],
+    }),
+    deleteServiceContract: build.mutation<void, number>({
+      query: (contractID) => ({
+        url: `/contract-service/${contractID}`,
+        method: "DELETE",
+      }),
+      invalidatesKeys: () => ["contract-service"],
+      onSuccess: () => {
+        translateToast({
+          status: "success",
+          titleKey: "toast-message-success",
+          descriptionKey: "toast-title",
+          translationNS: "serviceRenews",
+          keyPrefix: "delete",
+        });
+      },
+      onError: () => {
+        translateToast({
+          status: "error",
+          titleKey: "toast-message-error",
+          descriptionKey: "toast-title",
+          translationNS: "serviceRenews",
+          keyPrefix: "delete",
+        });
+      },
+    }),
+  }),
+});
+export const {
+  useAddServicesRenewMutation,
+  useUpdateServiceRenewMutation,
+  useDeleteServiceContractMutation,
+  useRenewServiceReminderMutation,
+  useServiceRenewForPeriodTimeQuery,
+  useServiceContractHistoryCustomerQuery,
+} = serviceRenewApi;
